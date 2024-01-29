2d52261d2487b22bf90ea48a8c5dd63ce4a2d05b
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-01-29 18:38:32 +0700

    gas

--- app/Http/Controllers/Accounting/JournalController.php
+++ app/Http/Controllers/Accounting/JournalController.php
@@ -815,8 +815,8 @@ class JournalController extends Controller
                     $pdf = Pdf::loadView('admin.print.accounting.journal_individual', $data)->setPaper('a4', 'landscape');
                     $pdf->render();
                     $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-                    $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-                    $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+                    $pdf->getCanvas()->page_text(650, 550, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
+                    $pdf->getCanvas()->page_text(650, 560, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
                     $content = $pdf->download()->getOriginalContent();
                     $temp_pdf[]=$content;
                 }
@@ -912,8 +912,8 @@ class JournalController extends Controller
                             $pdf = Pdf::loadView('admin.print.accounting.journal_individual', $data)->setPaper('a4', 'landscape');
                             $pdf->render();
                             $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-                            $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-                            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+                            $pdf->getCanvas()->page_text(650, 550, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
+                            $pdf->getCanvas()->page_text(650, 560, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
                             $content = $pdf->download()->getOriginalContent();
                             $temp_pdf[]=$content;
                            
@@ -988,8 +988,8 @@ class JournalController extends Controller
                             $pdf = Pdf::loadView('admin.print.accounting.journal_individual', $data)->setPaper('a4', 'landscape');
                             $pdf->render();
                             $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-                            $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-                            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+                            $pdf->getCanvas()->page_text(650, 550, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
+                            $pdf->getCanvas()->page_text(650, 560, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
                             $content = $pdf->download()->getOriginalContent();
                             $temp_pdf[]=$content;
                            
@@ -1087,7 +1087,7 @@ class JournalController extends Controller
                 
         if($cap){
             $data = [
-                'title'     => 'Print Journal',
+                'title'     => 'Approval Journal',
                 'data'      => $cap
             ];
 

ee63044b2370dec4260e34be7d9bd87e544e6be2
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-02-16 10:47:05 +0700

    gas

--- database/migrations/2024_02_16_104527_add_count_balance_to_assets_table.php
+++ database/migrations/2024_02_16_104527_add_count_balance_to_assets_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('assets', function (Blueprint $table) {
-            //
+            $table->integer('count_balance')->nullable()->after('book_balance');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('assets', function (Blueprint $table) {
-            //
+            $table->dropColumn('count_balance');
         });
     }
 };

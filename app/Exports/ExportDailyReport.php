18fca23559dbaf7b667eae1cf3ef2cf114d0d345
hartantioan <hartantioan@gmail.com>
2024-02-28 16:14:26 +0700

    Update ExportDailyReport.php

--- app/Exports/ExportDailyReport.php
+++ app/Exports/ExportDailyReport.php
@@ -8,10 +8,11 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use App\Models\User;
 use Carbon\Carbon;
 class ExportDailyReport implements FromView,ShouldAutoSize,WithTitle
 {
-   
+    protected $period_id;
     public function __construct(string $period_id)
     {
         $this->period_id = $period_id ? $period_id : '';
@@ -34,8 +35,15 @@ class ExportDailyReport implements FromView,ShouldAutoSize,WithTitle
         ->get();
         
         $distinctDates = $query_data_daily->pluck('date')->unique()->toArray();
+        $distinctUserIds = $query_data_daily->pluck('user_id')->unique()->toArray();
+        $distinctUsers = User::whereIn('id', $distinctUserIds)->get();
         $attendanceDetail=[];
+        $shift_per_date = [];
+        
+        $attendance_user = [];
         foreach($distinctDates as $key_date=>$row_dates){
+            $shifts_for_date = [];
+            $nama_shift = [];
             foreach($query_data_daily as $key_daily=>$row_daily){
                 if($row_daily['date']==$row_dates){
                     if($row_daily->shift != null){
@@ -45,7 +53,16 @@ class ExportDailyReport implements FromView,ShouldAutoSize,WithTitle
                         $min_time_in = "tidak ada shift";
                         $max_time_out = "tidak ada shift";
                     }
-                    
+                    if($row_daily->shift != null){
+                        if(!in_array($row_daily->shift->name, $nama_shift)){
+                            $nama_shift[]= $row_daily->shift->name;
+                            $shifts_for_date[]=[
+                                'nama'=> $row_daily->shift->name,
+                                'limit_masuk'=>$row_daily->shift->time_in ?? 'tidak ada shift',
+                                'limit_keluar'=>$row_daily->shift->time_out ?? 'tidak ada shift',
+                            ]; 
+                        }
+                    }
                     $attendanceDetail[$key_date][]=[
                         'user_id'=>$row_daily->user->employee_no??'',
                         'user_name'=>$row_daily->user->name??'',
@@ -59,14 +76,84 @@ class ExportDailyReport implements FromView,ShouldAutoSize,WithTitle
                         'date'=>$row_daily->date,
                         'status'=>$row_daily->rawStatus(),
                     ];
+                    
                 }
             }
+            $shift_per_date[]=[
+                'date' =>   $row_dates,
+                'shift'=>   $shifts_for_date,
+            ];
+            
             
         }
-
-      
+        foreach($shift_per_date as $row_shift){
+                
+            foreach($query_data_daily as $key_daily=>$row_daily){
+               
+                if($row_daily['date']==$row_shift['date']){
+                    if(count($row_shift['shift']) > 0){
+                        foreach($row_shift['shift'] as $row_shift_date){
+                            if($row_daily->shift()->exists()){
+                                if($row_daily->shift->name == $row_shift_date['nama']){
+                                    $attendance_user[$row_daily->user->id][]=[
+                                        'user_id'=>$row_daily->user->employee_no??'',
+                                        'user_name'=>$row_daily->user->name??'',
+                                        'nama_shift'=>$row_daily->shift->name ?? 'tidak ada shift',
+                                        'min_masuk'=>$min_time_in,
+                                        'max_keluar'=>$max_time_out,
+                                        'limit_masuk'=>$row_daily->shift->time_in ?? 'tidak ada shift',
+                                        'limit_keluar'=>$row_daily->shift->time_out ?? 'tidak ada shift',
+                                        'masuk'=>$row_daily->masuk,
+                                        'pulang'=>$row_daily->pulang,
+                                        'date'=>$row_daily->date,
+                                        'status'=>$row_daily->rawStatus(),
+                                    ];
+                                }
+                            }
+                            else{
+                                $attendance_user[$row_daily->user->id][]=[
+                                    'user_id'=>$row_daily->user->employee_no??'',
+                                    'user_name'=>$row_daily->user->name??'',
+                                    'nama_shift'=> 'tidak ada shift',
+                                    'min_masuk'=>'tidak ada shift',
+                                    'max_keluar'=>'tidak ada shift',
+                                    'limit_masuk'=>'tidak ada shift',
+                                    'limit_keluar'=>'tidak ada shift',
+                                    'masuk'=>'tidak ada shift',
+                                    'pulang'=>'tidak ada shift',
+                                    'date'=>$row_daily->date,
+                                    'status'=>$row_daily->rawStatus(),
+                                ];
+                            }
+                        }
+                    }else{
+                        $attendance_user[$row_daily->user->id][]=[
+                            'user_id'=>$row_daily->user->employee_no??'',
+                            'user_name'=>$row_daily->user->name??'',
+                            'nama_shift'=> 'tidak ada shift',
+                            'min_masuk'=>'tidak ada shift',
+                            'max_keluar'=>'tidak ada shift',
+                            'limit_masuk'=>'tidak ada shift',
+                            'limit_keluar'=>'tidak ada shift',
+                            'masuk'=>'tidak ada shift',
+                            'pulang'=>'tidak ada shift',
+                            'date'=>$row_daily->date,
+                            'status'=>$row_daily->rawStatus(),
+                        ];
+                    }
+                    
+                }
+            }
+        }
+        $distinctDatesCount = count($distinctDates);
+        
         return view('admin.exports.attendance_daily_report', [
             'data' => $attendanceDetail,
+            'date' => $distinctDates,
+            'user_id'=>$distinctUsers,
+            'attendanceUser'=>$attendance_user,
+            'shift_per_date' => $shift_per_date,
+            'distinctDatesCount' => $distinctDatesCount,
         ]);
     }
 }

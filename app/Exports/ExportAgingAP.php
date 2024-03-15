34faae9ea4a68b049387f39701fac8b7a63b41eb
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-03-15 19:07:51 +0700

    Update ExportAgingAP.php

--- app/Exports/ExportAgingAP.php
+++ app/Exports/ExportAgingAP.php
@@ -15,6 +15,9 @@ class ExportAgingAP implements FromView , WithEvents
     /**
     * @return \Illuminate\Support\Collection
     */
+
+    protected $date, $interval, $column, $type;
+
     public function __construct(string $date, int $interval, int $column, int $type)
     {
         $this->date = $date ? $date : '';
@@ -26,52 +29,8 @@ class ExportAgingAP implements FromView , WithEvents
     {
         $totalAll=0;
         $array_filter = [];
-        $query_data = DB::select("
-                SELECT 
-                    *,
-                    IFNULL((SELECT 
-                        SUM(nominal) 
-                        FROM payment_request_details prd 
-                        JOIN outgoing_payments op
-                            ON op.payment_request_id = prd.payment_request_id
-                        WHERE 
-                            prd.lookable_id = pi.id 
-                            AND prd.lookable_type = 'purchase_invoices'
-                            AND op.post_date <= :date1
-                            AND op.status IN ('2','3')
-                    ),0) AS total_payment,
-                    IFNULL((
-                        SELECT
-                            SUM(pmd.grandtotal)
-                            FROM purchase_memo_details pmd
-                            JOIN purchase_memos pm
-                                ON pm.id = pmd.purchase_memo_id
-                            JOIN purchase_invoice_details pid
-                                ON pid.purchase_invoice_id = pi.id
-                                AND pid.id = pmd.lookable_id
-                            WHERE pmd.lookable_type = 'purchase_invoice_details'
-                            AND pm.post_date <= :date2
-                            AND pm.status IN ('2','3')
-                    ),0) AS total_memo,
-                    u.name AS account_name,
-                    u.employee_no AS account_code
-                    FROM purchase_invoices pi
-                    LEFT JOIN users u
-                        ON u.id = pi.account_id
-                    WHERE 
-                        pi.post_date <= :date3
-                        AND pi.balance > 0
-                        AND pi.status IN ('2','3')
-            ", array(
-                'date1' => $this->date,
-                'date2' => $this->date,
-                'date3' => $this->date,
-            ));
-
-        $newData = [];
-
-        $query_data2 = DB::select("
-                SELECT 
+        $results = DB::select("
+            SELECT 
                 *,
                 IFNULL((SELECT 
                     SUM(nominal) 
@@ -80,9 +39,10 @@ class ExportAgingAP implements FromView , WithEvents
                         ON op.payment_request_id = prd.payment_request_id
                     WHERE 
                         prd.lookable_id = pi.id 
-                        AND prd.lookable_type = 'purchase_down_payments'
-                        AND op.post_date <= :date1
+                        AND prd.lookable_type = 'purchase_invoices'
+                        AND op.pay_date <= :date1
                         AND op.status IN ('2','3')
+                        AND prd.deleted_at IS NULL
                 ),0) AS total_payment,
                 IFNULL((
                     SELECT
@@ -93,24 +53,132 @@ class ExportAgingAP implements FromView , WithEvents
                         JOIN purchase_invoice_details pid
                             ON pid.purchase_invoice_id = pi.id
                             AND pid.id = pmd.lookable_id
-                        WHERE pmd.lookable_type = 'purchase_down_payments'
+                        WHERE pmd.lookable_type = 'purchase_invoice_details'
                         AND pm.post_date <= :date2
                         AND pm.status IN ('2','3')
+                        AND pmd.deleted_at IS NULL
                 ),0) AS total_memo,
+                IFNULL((
+                    SELECT
+                        SUM(prd.nominal)
+                        FROM payment_request_details prd
+                        JOIN payment_requests pr
+                            ON pr.id = prd.payment_request_id
+                        WHERE prd.lookable_type = 'purchase_invoices'
+                        AND prd.lookable_id = pi.id
+                        AND pr.post_date <= :date3
+                        AND pr.status IN ('2','3')
+                        AND pr.payment_type = '5'
+                        AND prd.deleted_at IS NULL
+                ),0) AS total_reconcile,
+                IFNULL((
+                    SELECT
+                        SUM(jd.nominal)
+                        FROM journal_details jd
+                        JOIN journals j
+                            ON j.id = jd.journal_id
+                        JOIN coas c
+                            ON jd.coa_id = c.id
+                        WHERE c.code = '200.01.03.01.01'
+                        AND jd.note = CONCAT('VOID*',pi.code)
+                        AND j.post_date <= :date4
+                        AND j.status IN ('2','3')
+                        AND jd.deleted_at IS NULL
+                ),0) AS total_journal,
                 u.name AS account_name,
-                u.employee_no AS account_code
+                u.employee_no AS account_code,
+                pi.code,
+                pi.post_date,
+                pi.received_date,
+                pi.due_date,
+                pi.total,
+                pi.tax,
+                pi.wtax,
+                pi.balance
+                FROM purchase_invoices pi
+                LEFT JOIN users u
+                    ON u.id = pi.account_id
+                WHERE 
+                    pi.post_date <= :date5
+                    AND pi.balance > 0
+                    AND pi.status IN ('2','3')
+                    AND pi.deleted_at IS NULL
+        ", array(
+            'date1' => $this->date,
+            'date2' => $this->date,
+            'date3' => $this->date,
+            'date4' => $this->date,
+            'date5' => $this->date,
+        ));
+
+        $results2 = DB::select("
+            SELECT 
+                *,
+                pi.top AS topdp,
+                IFNULL((SELECT 
+                    SUM(nominal) 
+                    FROM payment_request_details prd 
+                    JOIN outgoing_payments op
+                        ON op.payment_request_id = prd.payment_request_id
+                    WHERE 
+                        prd.lookable_id = pi.id 
+                        AND prd.lookable_type = 'purchase_down_payments'
+                        AND op.pay_date <= :date1
+                        AND op.status IN ('2','3')
+                        AND prd.deleted_at IS NULL
+                ),0) AS total_payment,
+                IFNULL((
+                    SELECT
+                        SUM(pmd.grandtotal)
+                        FROM purchase_memo_details pmd
+                        JOIN purchase_memos pm
+                            ON pm.id = pmd.purchase_memo_id
+                        WHERE pmd.lookable_type = 'purchase_down_payments'
+                        AND pmd.lookable_id = pi.id
+                        AND pm.post_date <= :date2
+                        AND pm.status IN ('2','3')
+                        AND pmd.deleted_at IS NULL
+                ),0) AS total_memo,
+                IFNULL((
+                    SELECT
+                        SUM(prd.nominal)
+                        FROM payment_request_details prd
+                        JOIN payment_requests pr
+                            ON pr.id = prd.payment_request_id
+                        WHERE prd.lookable_type = 'purchase_down_payments'
+                        AND prd.lookable_id = pi.id
+                        AND pr.post_date <= :date3
+                        AND pr.status IN ('2','3')
+                        AND pr.payment_type = '5'
+                        AND prd.deleted_at IS NULL
+                ),0) AS total_reconcile,
+                u.name AS account_name,
+                u.employee_no AS account_code,
+                pi.code,
+                pi.post_date,
+                pi.document_date,
+                pi.due_date,
+                pi.total,
+                pi.tax,
+                pi.wtax,
+                pi.grandtotal,
+                pi.currency_rate
                 FROM purchase_down_payments pi
                 LEFT JOIN users u
                     ON u.id = pi.account_id
                 WHERE 
-                    pi.post_date <= :date3
+                    pi.post_date <= :date4
                     AND pi.grandtotal > 0
                     AND pi.status IN ('2','3')
-            ", array(
-                'date1' => $this->date,
-                'date2' => $this->date,
-                'date3' => $this->date,
-            ));
+                    AND pi.deleted_at IS NULL
+        ", array(
+            'date1' => $this->date,
+            'date2' => $this->date,
+            'date3' => $this->date,
+            'date4' => $this->date,
+        ));
+
+        $newData = [];
 
         $countPeriod = 1;
         $column = intval($this->column);
@@ -144,7 +212,7 @@ class ExportAgingAP implements FromView , WithEvents
         $newData = [];
 
         if($this->type == 1){
-            foreach($query_data as $row){
+            foreach($results as $row){
                 $balance = $row->balance - $row->total_payment - $row->total_memo;
                 if($balance > 0){
                     $totalAll += $balance;
@@ -191,7 +259,7 @@ class ExportAgingAP implements FromView , WithEvents
                 }
             }
     
-            foreach($query_data2 as $row){
+            foreach($results2 as $row){
                 $balance = $row->grandtotal - $row->total_payment - $row->total_memo;
                 if($balance > 0){
                     $totalAll += $balance;
@@ -245,7 +313,7 @@ class ExportAgingAP implements FromView , WithEvents
                 'totalall'      => $totalAll
             ]);
         }else{
-            foreach($query_data as $row){
+            foreach($results as $row){
                 $balance = $row->balance - $row->total_payment - $row->total_memo;
                 if($balance > 0){
                     $daysDiff = $this->dateDiffInDays($row->due_date,$this->date);
@@ -279,7 +347,7 @@ class ExportAgingAP implements FromView , WithEvents
                 }
             }
     
-            foreach($query_data2 as $row){
+            foreach($results2 as $row){
                 $balance = $row->grandtotal - $row->total_payment - $row->total_memo;
                 if($balance > 0){
                     $daysDiff = $this->dateDiffInDays($row->due_date,$this->date);

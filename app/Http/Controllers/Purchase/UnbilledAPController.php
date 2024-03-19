9e3836431fcc0e1a16116b81603124396d7d3a7f
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-03-19 08:45:23 +0700

    Update UnbilledAPController.php

--- app/Http/Controllers/Purchase/UnbilledAPController.php
+++ app/Http/Controllers/Purchase/UnbilledAPController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Menu;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
 
 class UnbilledAPController extends Controller
 {
@@ -24,5 +26,27 @@ class UnbilledAPController extends Controller
 
         return view('admin.layouts.index', ['data' => $data]);
 
+    }
+
+    public function filterByDate(Request $request){
+        $array_filter = [];
+        $start_date = $request->start_date;
+        $end_date = $request->end_date;
+
+        $results = DB::select("
+            SELECT 
+                *
+                FROM good_receipts gr
+                LEFT JOIN users u
+                    ON u.id = gr.account_id
+                WHERE 
+                    gr.post_date <= :dateend
+                    AND gr.post_date >= :datestart
+                    AND gr.status IN ('2','3')
+                    AND gr.deleted_at IS NULL
+        ", array(
+            'datestart' => $start_date,
+            'dateend'   => $end_date,
+        ));
     }
 }

b94b9e1a1657f33fab45527da787f73f44e4d1d3
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-03-25 12:16:39 +0700

    Update PurchaseProgressController.php

--- app/Http/Controllers/Purchase/PurchaseProgressController.php
+++ app/Http/Controllers/Purchase/PurchaseProgressController.php
@@ -283,7 +283,7 @@ class PurchaseProgressController extends Controller
     
 
         // Generate the HTML for the table
-        $tableHtml = '<table border="1" class="table-bordered">';
+        $tableHtml = '<table border="1" class="bordered">';
         $tableHtml .= '<thead>';
         $tableHtml .= '<tr>';
         $tableHtml .= '<th>Item</th>';

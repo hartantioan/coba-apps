aa0ad80741757896b58fab6a17320cc35980e533
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-04-02 14:32:55 +0700

    Update SubsidiaryLedgerController.php

--- app/Http/Controllers/Accounting/SubsidiaryLedgerController.php
+++ app/Http/Controllers/Accounting/SubsidiaryLedgerController.php
@@ -98,7 +98,7 @@ class SubsidiaryLedgerController extends Controller
             $html .= '<tr style="font-weight:800;">
                             <td width="200px">'.$row->code.'</td>
                             <td width="200px">'.$row->name.'</td>
-                            <td colspan="5"></td>
+                            <td colspan="7"></td>
                             <td class="right-align">'.number_format($balance,2,',','.').'</td>
                             <td colspan="11"></td>
                         </tr>';

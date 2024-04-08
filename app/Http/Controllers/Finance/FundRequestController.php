c3840f1e9e04a982b02a85b8e3df9d874928a7aa
hartantioan <hartantioan@gmail.com>
2024-04-08 14:52:46 +0700

    Update FundRequestController.php

--- app/Http/Controllers/Finance/FundRequestController.php
+++ app/Http/Controllers/Finance/FundRequestController.php
@@ -798,9 +798,9 @@ class FundRequestController extends Controller
             $pdf->render();
     
             $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-            $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $pr->printCounter()->count(), $font, 10, array(0,0,0));
-            $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+            $pdf->getCanvas()->page_text(495, 760, "Jumlah Print, ". $pr->printCounter()->count(), $font, 10, array(0,0,0));
+            $pdf->getCanvas()->page_text(505, 770, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
+            $pdf->getCanvas()->page_text(422, 790, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
             
             $content = $pdf->download()->getOriginalContent();
             

c82c11f09ed12fb74bd2225a8cdd367024886dfc
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-04-16 10:57:56 +0700

    Update DownPaymentController.php

--- app/Http/Controllers/Purchase/DownPaymentController.php
+++ app/Http/Controllers/Purchase/DownPaymentController.php
@@ -120,7 +120,7 @@ class DownPaymentController extends Controller
                     'used'          => number_format($row->total_used * $currency_rate,2,',','.'),
                     'memo'          => number_format($row->total_memo * $currency_rate,2,',','.'),
                     'balance'       => number_format($balance * $currency_rate,2,',','.'),
-                    'balance_fc'     => number_format($balance,2,',','.'),
+                    'balance_fc'    => number_format($balance,2,',','.'),
                 ];
                 $totalbalance += ($balance * $currency_rate);
             }

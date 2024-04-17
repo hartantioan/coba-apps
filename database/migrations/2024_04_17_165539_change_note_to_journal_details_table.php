260da857b0392bdd0f7fb51735f142d01cbca465
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-04-17 16:56:54 +0700

    Create 2024_04_17_165539_change_note_to_journal_details_table.php

---
+++ database/migrations/2024_04_17_165539_change_note_to_journal_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('journal_details', function (Blueprint $table) {
+            $table->string('note',5000)->nullable()->change();
+            $table->string('note2',5000)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('journal_details', function (Blueprint $table) {
+            //
+        });
+    }
+};

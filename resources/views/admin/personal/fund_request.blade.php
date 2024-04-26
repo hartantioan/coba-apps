2b8d402ca13286bed00775f11568aa1832025323
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-04-26 13:24:53 +0700

    Update fund_request.blade.php

--- resources/views/admin/personal/fund_request.blade.php
+++ resources/views/admin/personal/fund_request.blade.php
@@ -1040,6 +1040,7 @@
                     passedLimit = false;
                 } */
                 let passedUpload = true;
+                let passedFormDetail = true;
                 var files = document.getElementById('file');
 
                 if(files.files.length > 0){
@@ -1068,6 +1069,12 @@
                             formData.append('arr_project[]',($(this).val() ? $(this).val() : ''));
                         });
 
+                        $('select[name^="arr_unit[]"]').each(function(index){
+                            if(!$(this).val()){
+                                passedFormDetail = false;
+                            }
+                        });
+
                         $('input[name^="arr_checklist_box[]"]').each(function(index){
                             if($(this).is(':checked')){
                                 formData.append('arr_checklist_box[]',$(this).val());
@@ -1075,75 +1082,83 @@
                             }
                         });
                         
-                        $.ajax({
-                            url: '{{ Request::url() }}/create',
-                            type: 'POST',
-                            dataType: 'JSON',
-                            data: formData,
-                            contentType: false,
-                            processData: false,
-                            cache: true,
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                            },
-                            beforeSend: function() {
-                                $('#validation_alert').hide();
-                                $('#validation_alert').html('');
-                                loadingOpen('#modal1');
-                            },
-                            success: function(response) {
-                                loadingClose('#modal1');
-                                $('input').css('border', 'none');
-                                $('input').css('border-bottom', '0.5px solid black');
-                                if(response.status == 200) {
-                                    success();
-                                    M.toast({
-                                        html: response.message
-                                    });
-                                } else if(response.status == 422) {
-                                    $('#validation_alert').show();
-                                    $('.modal-content').scrollTop(0);
-                                    $.each(response.error, function(field, errorMessage) {
-                                        $('#' + field).addClass('error-input');
-                                        $('#' + field).css('border', '1px solid red');
-                                        
-                                    });
-                                    swal({
-                                        title: 'Ups! Validation',
-                                        text: 'Check your form.',
-                                        icon: 'warning'
-                                    });
-
-                                    $.each(response.error, function(i, val) {
-                                        $.each(val, function(i, val) {
-                                            $('#validation_alert').append(`
-                                                <div class="card-alert card red">
-                                                    <div class="card-content white-text">
-                                                        <p>` + val + `</p>
-                                                    </div>
-                                                    <button type="button" class="close white-text" data-dismiss="alert" aria-label="Close">
-                                                        <span aria-hidden="true">×</span>
-                                                    </button>
-                                                </div>
-                                            `);
+                        if(passedFormDetail){
+                            $.ajax({
+                                url: '{{ Request::url() }}/create',
+                                type: 'POST',
+                                dataType: 'JSON',
+                                data: formData,
+                                contentType: false,
+                                processData: false,
+                                cache: true,
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                },
+                                beforeSend: function() {
+                                    $('#validation_alert').hide();
+                                    $('#validation_alert').html('');
+                                    loadingOpen('#modal1');
+                                },
+                                success: function(response) {
+                                    loadingClose('#modal1');
+                                    $('input').css('border', 'none');
+                                    $('input').css('border-bottom', '0.5px solid black');
+                                    if(response.status == 200) {
+                                        success();
+                                        M.toast({
+                                            html: response.message
                                         });
-                                    });
-                                } else {
-                                    M.toast({
-                                        html: response.message
+                                    } else if(response.status == 422) {
+                                        $('#validation_alert').show();
+                                        $('.modal-content').scrollTop(0);
+                                        $.each(response.error, function(field, errorMessage) {
+                                            $('#' + field).addClass('error-input');
+                                            $('#' + field).css('border', '1px solid red');
+                                            
+                                        });
+                                        swal({
+                                            title: 'Ups! Validation',
+                                            text: 'Check your form.',
+                                            icon: 'warning'
+                                        });
+
+                                        $.each(response.error, function(i, val) {
+                                            $.each(val, function(i, val) {
+                                                $('#validation_alert').append(`
+                                                    <div class="card-alert card red">
+                                                        <div class="card-content white-text">
+                                                            <p>` + val + `</p>
+                                                        </div>
+                                                        <button type="button" class="close white-text" data-dismiss="alert" aria-label="Close">
+                                                            <span aria-hidden="true">×</span>
+                                                        </button>
+                                                    </div>
+                                                `);
+                                            });
+                                        });
+                                    } else {
+                                        M.toast({
+                                            html: response.message
+                                        });
+                                    }
+                                },
+                                error: function() {
+                                    $('.modal-content').scrollTop(0);
+                                    loadingClose('#modal1');
+                                    swal({
+                                        title: 'Ups!',
+                                        text: 'Check your internet connection.',
+                                        icon: 'error'
                                     });
                                 }
-                            },
-                            error: function() {
-                                $('.modal-content').scrollTop(0);
-                                loadingClose('#modal1');
-                                swal({
-                                    title: 'Ups!',
-                                    text: 'Check your internet connection.',
-                                    icon: 'error'
-                                });
-                            }
-                        });
+                            });
+                        }else{
+                            swal({
+                                title: 'Ups! Maaf.',
+                                text: 'Unit tidak boleh kosong.',
+                                icon: 'warning'
+                            });
+                        }
                     }else{
                         swal({
                             title: 'Ups! Maaf.',

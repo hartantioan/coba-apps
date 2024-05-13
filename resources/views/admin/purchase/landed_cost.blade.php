61265ceebf83d13883a4a280483359bbc05d422a
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-05-13 08:32:37 +0700

    Update landed_cost.blade.php

--- resources/views/admin/purchase/landed_cost.blade.php
+++ resources/views/admin/purchase/landed_cost.blade.php
@@ -2030,7 +2030,7 @@
             $('input[name^="arr_total"]').each(function(index){
                 let totalrow = (parseFloat($(this).val()) / totalall) * total;
                 $('input[name^="arr_price"]:eq(' + index + ')').val(
-                    (totalrow >= 0 ? '' : '-') + formatRupiahIni(totalrow.toFixed(2).toString().replace('.',','))
+                    (totalrow >= 0 ? '' : '-') + formatRupiahIni(totalrow.toFixed(5).toString().replace('.',','))
                 );
             })
         }

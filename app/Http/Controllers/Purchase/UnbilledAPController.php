69ab5163fb4497cb5746b97872d955e78e55f88e
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-06-10 20:33:28 +0700

    Update UnbilledAPController.php

--- app/Http/Controllers/Purchase/UnbilledAPController.php
+++ app/Http/Controllers/Purchase/UnbilledAPController.php
@@ -41,7 +41,7 @@ class UnbilledAPController extends Controller
             gr.*,
             u.name AS account_name,
             IFNULL((SELECT 
-                SUM(round(pid.total * pi.currency_rate,2))
+                SUM(pid.total)
                 FROM purchase_invoice_details pid
                 JOIN purchase_invoices pi
                     ON pi.id = pid.purchase_invoice_id
@@ -144,12 +144,13 @@ class UnbilledAPController extends Controller
                         $query->where('code','200.01.03.01.02');
                     })->whereHas('journal',function($query)use($date){
                         $query->where('post_date','<=',$date)->whereIn('status',['2','3']);
-                    })->sum('nominal');
+                    })->sum('nominal_fc');
                 }
             }
+            $balance = round($row->total - ($row->total_invoice - $total_reconcile) - $row->total_return,2);
             $currency_rate = $row->currency_rate;
             $total_received_after_adjust = round(($row->total * $currency_rate) + $row->adjust_nominal,2);
-            $total_invoice_after_adjust = round($row->total_invoice - $total_reconcile + round($row->total_return * $currency_rate,2),2);
+            $total_invoice_after_adjust = round(($row->total_invoice - $total_reconcile + $row->total_return) * $currency_rate,2);
             $balance_after_adjust = round($total_received_after_adjust - $total_invoice_after_adjust,2);
             if(round($balance_after_adjust,2) > 0){
                 $array_filter[] = [

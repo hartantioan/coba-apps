6a1493907d452c6acb970b73e816f6b2d123b5e2
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-06-27 10:10:39 +0700

    gas

--- app/Http/Controllers/Inventory/GoodIssueRequestController.php
+++ app/Http/Controllers/Inventory/GoodIssueRequestController.php
@@ -407,12 +407,12 @@ class GoodIssueRequestController extends Controller
         
         if($query) {
 
-            if(!CustomHelper::checkLockAcc($query->post_date)){
+            /* if(!CustomHelper::checkLockAcc($query->post_date)){
                 return response()->json([
                     'status'  => 500,
                     'message' => 'Transaksi pada periode dokumen telah ditutup oleh Akunting. Anda tidak bisa melakukan perubahan.'
                 ]);
-            }
+            } */
 
             if(in_array($query->status,['4','5'])){
                 $response = [
--- app/Http/Controllers/Purchase/PurchaseRequestController.php
+++ app/Http/Controllers/Purchase/PurchaseRequestController.php
@@ -422,12 +422,12 @@ class PurchaseRequestController extends Controller
         
         if($query) {
 
-            if(!CustomHelper::checkLockAcc($query->post_date)){
+            /* if(!CustomHelper::checkLockAcc($query->post_date)){
                 return response()->json([
                     'status'  => 500,
                     'message' => 'Transaksi pada periode dokumen telah ditutup oleh Akunting. Anda tidak bisa melakukan perubahan.'
                 ]);
-            }
+            } */
 
             if(in_array($query->status,['4','5'])){
                 $response = [

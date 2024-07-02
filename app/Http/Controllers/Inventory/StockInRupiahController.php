f230732c5862ce0bd9422bc189afc56ac330b809
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-02 07:22:35 +0700

    Update StockInRupiahController.php

--- app/Http/Controllers/Inventory/StockInRupiahController.php
+++ app/Http/Controllers/Inventory/StockInRupiahController.php
@@ -83,7 +83,7 @@ class StockInRupiahController extends Controller
             $query_data = ItemCogs::whereIn('id', function ($query) use ($request) {            
                 $query->selectRaw('MAX(id)')
                     ->from('item_cogs')
-                    ->where('date', '<=', $request->finish_date)
+                    /* ->where('date', '<=', $request->finish_date) */
                     ->groupBy('item_id');
             })
             ->where(function($query) use ( $request) {

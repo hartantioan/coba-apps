8b621c993cbbbec456603420e0687472612d6a26
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-04 15:32:38 +0700

    Update order.blade.php

--- resources/views/admin/purchase/order.blade.php
+++ resources/views/admin/purchase/order.blade.php
@@ -3071,9 +3071,9 @@
 
         $('.arr_subtotal').each(function(index){
             let rownominal = parseFloat($(this).text().replaceAll(".", "").replaceAll(",",".")), rowtax = 0, rowwtax = 0, rowbobot = 0, rowdiscount = 0, rowgrandtotal = 0;
-            rowbobot = (rownominal / subtotal) * 100;
+            rowbobot = Math.round(((rownominal / subtotal) + Number.EPSILON) * 100) / 100;
             console.log(rowbobot);
-            rowdiscount = (discount * rowbobot) / 100;
+            rowdiscount = discount * rowbobot;
             rownominal -= rowdiscount;
 
             if($('select[name^="arr_tax"]').eq(index).val() !== '0'){

0056ea84fb80296eb44eb9fbe435bd77469d45a5
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-09 09:36:44 +0700

    Update PurchaseOrderController.php

--- app/Http/Controllers/Purchase/PurchaseOrderController.php
+++ app/Http/Controllers/Purchase/PurchaseOrderController.php
@@ -890,6 +890,8 @@ class PurchaseOrderController extends Controller
                             $row->delete();
                         }
 
+                        CustomHelper::removeApproval($query->getTable(),$query->id);
+
                         DB::commit();
                     }else{
                         return response()->json([

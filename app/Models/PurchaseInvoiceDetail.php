c61795c644d0a1ab5782069a3d832a687c1a0593
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-15 15:12:37 +0700

    Update PurchaseInvoiceDetail.php

--- app/Models/PurchaseInvoiceDetail.php
+++ app/Models/PurchaseInvoiceDetail.php
@@ -154,7 +154,7 @@ class PurchaseInvoiceDetail extends Model
             'good_receipt_details'      => $this->lookable->item->code.' - '.$this->lookable->item->name,
             'landed_cost_fee_details'   => $this->lookable->landedCostFee->name,
             'purchase_order_details'    => $this->lookable->item_id ? $this->lookable->item->code.' - '.$this->lookable->item->name : $this->lookable->coa->code.' - '.$this->lookable->coa->name,
-            'coas'                      => $this->lookable->name,
+            'coas'                      => ($this->lookable->prefix ? $this->lookable->prefix.' - ' : '').$this->lookable->name,
             default                     => '-',
         };
 

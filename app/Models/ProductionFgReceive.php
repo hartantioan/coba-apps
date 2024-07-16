2f77bedc6ff16de6e2379d4e5e2d76a52a3bc7d1
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-16 12:53:10 +0700

    Update ProductionFgReceive.php

--- app/Models/ProductionFgReceive.php
+++ app/Models/ProductionFgReceive.php
@@ -311,9 +311,9 @@ class ProductionFgReceive extends Model
                 'qty_bom'                       => 0,
                 'nominal_bom'                   => 0,
                 'total_bom'                     => 0,
-                'qty_planned'                   => 0,
-                'nominal_planned'               => 0,
-                'total_planned'                 => 0,
+                'qty_planned'                   => $row->qty,
+                'nominal_planned'               => $price,
+                'total_planned'                 => $rowtotal,
                 'from_item_stock_id'            => $itemStock->id,
             ]);
             if($query){
@@ -364,9 +364,9 @@ class ProductionFgReceive extends Model
                 'qty_bom'                       => 0,
                 'nominal_bom'                   => 0,
                 'total_bom'                     => 0,
-                'qty_planned'                   => 0,
-                'nominal_planned'               => 0,
-                'total_planned'                 => 0,
+                'qty_planned'                   => $row->qty,
+                'nominal_planned'               => $price,
+                'total_planned'                 => $total,
                 'from_item_stock_id'            => $itemStock->id,
                 'is_wip'                        => '1',
             ]);
@@ -408,9 +408,9 @@ class ProductionFgReceive extends Model
                         'qty_bom'                       => 0,
                         'nominal_bom'                   => 0,
                         'total_bom'                     => 0,
-                        'qty_planned'                   => 0,
-                        'nominal_planned'               => 0,
-                        'total_planned'                 => 0,
+                        'qty_planned'                   => round($rowbom->qty * ($row->qty / $rowbom->bom->qty_output),3),
+                        'nominal_planned'               => $nominal,
+                        'total_planned'                 => $total,
                         'from_item_stock_id'            => $itemstock ? $itemstock->id : NULL,
                     ]);
                 }

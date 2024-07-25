1e1047e2059060831eac60078a828464060979a0
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-25 18:16:31 +0700

    Update hardware_item_barcode.blade.php

--- resources/views/admin/print/usage/hardware_item_barcode.blade.php
+++ resources/views/admin/print/usage/hardware_item_barcode.blade.php
@@ -18,9 +18,9 @@
         <main style="padding: 0.1cm">
             
             <div style="font-size: 6px">
-                <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($data->code, 'C128')}}" alt="barcode" style="position: fixed;bottom:40px;right: -35px;width:4.5cm;height:0.5cm" />
-                <img src="{{ $image }}" style="position: fixed;top:35px;left:-40px; width:40%;">
-                <div style="position: fixed;bottom:19px;left:25px;font-size:10px;width:100%;font-weight:bold;">{{ $data->code }} - {{$data->item}}</div>
+                <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($data->code, 'C128')}}" alt="barcode" style="position: fixed;bottom:30px;right: -35px;width:4.5cm;height:0.9cm" />
+                <img src="{{ $image }}" style="position: fixed;top:40px;left:-40px; width:40%;">
+                <div style="position: fixed;bottom:10px;left:25px;font-size:10px;width:100%;font-weight:bold;">{{ $data->code }} - {{$data->item}}</div>
                 <div style="position: fixed;bottom:-2px;left:25px;font-size:8px;width:2cm">{{ (strlen($data->detail1) > 20) ? substr($data->detail1, 0, 20).'...' : $data->detail1 }}</div>
                 <div style="position: fixed; top:35px;left:-25px; width:200%; margin-top: 50%;font-size:9px;font-weight:bold">PT SUPERIOR PORCELAIN SUKSES</div>
 

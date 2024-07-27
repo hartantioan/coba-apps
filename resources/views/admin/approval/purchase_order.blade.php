9650bf5b547bd0fc6339367f3398ac7a077c4bce
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-07-27 09:50:42 +0700

    Update purchase_order.blade.php

--- resources/views/admin/approval/purchase_order.blade.php
+++ resources/views/admin/approval/purchase_order.blade.php
@@ -299,7 +299,7 @@
                                 @if($row->item->historyPurchaseOrderByPlace()->exists())
                                     <ol>
                                     @foreach($row->item->historyPurchaseOrderByPlace as $rowitem)
-                                        <li>Supplier : {{ $rowitem->purchaseOrder->account->name }} Tgl. {{ date('d/m/Y',strtotime($rowitem->purchaseOrder->post_date)) }} Harga @ : {{ CustomHelper::formatConditionQty($rowitem->price) }}</li>
+                                        <li>Supplier : {{ $rowitem->purchaseOrder->account->name }} Tgl. {{ date('d/m/Y',strtotime($rowitem->purchaseOrder->post_date)) }} Harga @ : {{ CustomHelper::formatConditionalQty($rowitem->price) }}</li>
                                     @endforeach
                                     </ol>
                                 @endif

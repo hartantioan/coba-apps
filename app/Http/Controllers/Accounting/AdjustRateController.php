9255baef44188e6af3d045fa6ddec01b59ae2a66
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-05 11:03:17 +0700

    Update AdjustRateController.php

--- app/Http/Controllers/Accounting/AdjustRateController.php
+++ app/Http/Controllers/Accounting/AdjustRateController.php
@@ -247,7 +247,7 @@ class AdjustRateController extends Controller
 
             foreach($datagr as $row){
                 $latest_rate = $row->latestCurrencyRateByDate($request->post_date);
-                $total = $row->balanceTotal();
+                $total = round($row->balanceTotal(),2);
                 if($total > 0){
                     $result[] = [
                         'coa_id'        => $coahutangusahabelumditagih->id,

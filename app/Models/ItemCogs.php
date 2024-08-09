1ecc239dfb6dc01a53051a6608a5ae891aac58dc
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-09 16:16:40 +0700

    Update ItemCogs.php

--- app/Models/ItemCogs.php
+++ app/Models/ItemCogs.php
@@ -77,7 +77,12 @@ class ItemCogs extends Model
     public function getRequester(){
         $requester = '-';
         if($this->lookable_type == 'good_receipts' || $this->lookable_type == 'good_issues'){
-            $requester = $this->detailable->requester ?? 'TIDAK DIISI';
+            if($this->lookable_type == 'good_issues'){
+                $requester = $this->detailable->requester ?? 'TIDAK DIISI';
+            }
+            if($this->lookable_type == 'good_receipts'){
+                $requester = $this->detailable->purchaseOrderDetail->requester ?? 'TIDAK DIISI';
+            }
         }
         return $requester;
     }

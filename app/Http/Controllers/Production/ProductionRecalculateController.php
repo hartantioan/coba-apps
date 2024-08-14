393e3e8a98798fb7d8a7824086fca16622269c14
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-14 09:19:09 +0700

    Update ProductionRecalculateController.php

--- app/Http/Controllers/Production/ProductionRecalculateController.php
+++ app/Http/Controllers/Production/ProductionRecalculateController.php
@@ -48,7 +48,7 @@ class ProductionRecalculateController extends Controller
        
         $menu = Menu::where('url', $lastSegment)->first();
         $data = [
-            'title'         => 'Serah Terima',
+            'title'         => 'Rekalkulasi Produksi',
             'content'       => 'admin.production.recalculate',
             'company'       => Company::where('status','1')->get(),
             'place'         => Place::where('status','1')->whereIn('id',$this->dataplaces)->get(),

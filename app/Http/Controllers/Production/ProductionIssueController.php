371c28848a19de885ba18aece542ca8635d796ce
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-15 09:23:30 +0700

    gas

--- app/Http/Controllers/Production/ProductionIssueController.php
+++ app/Http/Controllers/Production/ProductionIssueController.php
@@ -274,10 +274,10 @@ class ProductionIssueController extends Controller
                         if($request->arr_batch_index && in_array($key,$request->arr_batch_index)){
                             foreach($request->arr_batch_index as $keybatch => $rowbatch){
                                 if($key == $rowbatch){
-                                    $itemstock = $item->itemStock()->where('place_id',$request->arr_place[$key])->where('warehouse_id',$request->arr_warehouse[$key])->where('production_batch_id',$request->arr_batch_id[$keybatch])->first();
+                                    $itemstock = $item->itemCogs()->where('place_id',$request->arr_place[$key])->where('warehouse_id',$request->arr_warehouse[$key])->where('production_batch_id',$request->arr_batch_id[$keybatch])->orderByDesc('date')->orderByDesc('id')->first();
                                     $qty = str_replace(',','.',str_replace('.','',$request->arr_qty_batch[$keybatch]));
                                     if($itemstock){
-                                        if(round($qty,3) > $itemstock->qty){
+                                        if(round($qty,3) > $itemstock->qty_final){
                                             $passedQty = false;
                                             $arrNotPassedQty[] = $itemstock->item->code.' - '.$itemstock->item->name;
                                         }
@@ -285,10 +285,10 @@ class ProductionIssueController extends Controller
                                 }
                             }
                         }else{
-                            $itemstock = $item->itemStock()->where('place_id',$request->arr_place[$key])->where('warehouse_id',$request->arr_warehouse[$key])->first();
+                            $itemstock = $item->itemCogs()->where('place_id',$request->arr_place[$key])->where('warehouse_id',$request->arr_warehouse[$key])->orderByDesc('date')->orderByDesc('id')->first();
                             $qty = str_replace(',','.',str_replace('.','',$row));
                             if($itemstock){
-                                if(round($qty,3) > $itemstock->qty){
+                                if(round($qty,3) > $itemstock->qty_final){
                                     $passedQty = false;
                                     $arrNotPassedQty[] = $itemstock->item->code.' - '.$itemstock->item->name;
                                 }

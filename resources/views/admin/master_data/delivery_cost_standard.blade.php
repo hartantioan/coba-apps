e4b63602d320f1394ef95757d723ecf27fe1e42c
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-08-25 13:16:00 +0700

    Update delivery_cost_standard.blade.php

--- resources/views/admin/master_data/delivery_cost_standard.blade.php
+++ resources/views/admin/master_data/delivery_cost_standard.blade.php
@@ -131,7 +131,7 @@
     <div class="modal-content">
         <div class="row">
             <div class="col s12">
-                <h4>{{ __('translations.add') }}/{{ __('translations.edit') }} Standar Harga Pelanggan</h4>
+                <h4>{{ __('translations.add') }}/{{ __('translations.edit') }} {{ $title }}</h4>
                 <form class="row" id="form_data" onsubmit="return false;">
                     <div class="col s12">
                         <div id="validation_alert" style="display:none;"></div>

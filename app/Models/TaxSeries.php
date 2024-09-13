bfc5f024f68bd3f031e82f0d9a7420e788e47d19
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-13 14:29:09 +0700

    Update TaxSeries.php

--- app/Models/TaxSeries.php
+++ app/Models/TaxSeries.php
@@ -78,6 +78,7 @@ class TaxSeries extends Model
         $dataInvoice = MarketingOrderInvoice::whereIn('status',['2','3','5'])->where('company_id',$company_id)->whereRaw("SUBSTRING(tax_no,8,2) = '$year'")->whereNotNull('tax_no')->pluck('tax_no')->toArray();
         $dataDp = MarketingOrderDownPayment::whereIn('status',['2','3','5'])->where('company_id',$company_id)->whereRaw("SUBSTRING(tax_no,8,2) = '$year'")->whereNotNull('tax_no')->pluck('tax_no')->toArray();
         $newList = array_merge($dataInvoice,$dataDp);
+        $newList = array_unique($newList);
         rsort($newList);
         return $newList;
     } 
@@ -95,7 +96,7 @@ class TaxSeries extends Model
                 $startno = intval($data->start_no);
                 $endno = intval($data->end_no);
                 if($currentno >= $startno && $currentno <= $endno){
-                    $newcurrent = str_pad($currentno, 8, 0, STR_PAD_LEFT);
+                    $newcurrent = $currentno/* str_pad($currentno, 8, 0, STR_PAD_LEFT) */;
                     $no = $prefix.'.'.$data->branch_code.'.'.$data->year.'.'.$newcurrent;
                     $response = [
                         'status'    => 200,

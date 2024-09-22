3fe6119e7a15121b6cf830015fdbafe48648cc9d
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-22 18:53:32 +0700

    Update order_invoice_individual.blade.php

--- resources/views/admin/print/sales/order_invoice_individual.blade.php
+++ resources/views/admin/print/sales/order_invoice_individual.blade.php
@@ -247,7 +247,7 @@
                                 @foreach($data->marketingOrderInvoiceDeliveryProcessDetail as $key => $row)
                                 <tr>
                                     <td style="width:5% !important">{{ ($key + 1) }}</td>
-                                    <td style="width:40% !important;padding-left:5px">{{ $row->lookable->itemStock->item->code.' - '.$row->lookable->itemStock->item->name }}</td>
+                                    <td style="width:40% !important;padding-left:5px">{{ $row->lookable->itemStock->item->name.' ( '.CustomHelper::formatConditionalQty($row->qty * $row->lookable->itemStock->item->pallet->box_conversion).' BOX )' }}</td>
                                     <td style="width:10% !important;padding-left:20px" align="right">{{ CustomHelper::formatConditionalQty($row->lookable->qty) }}</td>
                                     <td style="width:5% !important" align="right">{{ CustomHelper::formatConditionalQty(round($row->lookable->qty * $row->lookable->marketingOrderDeliveryDetail->marketingOrderDetail->qty_conversion,3)) }}</td>
                                     <td style="width:5% !important"align="right">{{ $row->lookable->itemStock->item->uomUnit->code }}</td>

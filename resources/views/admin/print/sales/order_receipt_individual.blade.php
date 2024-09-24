9390ecfedbcf88425e2455d5b54c8b73862c9940
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-24 14:09:10 +0700

    Update order_receipt_individual.blade.php

--- resources/views/admin/print/sales/order_receipt_individual.blade.php
+++ resources/views/admin/print/sales/order_receipt_individual.blade.php
@@ -50,7 +50,7 @@
             @media only screen and (max-width : 992px) {
                 .invoice-print-area {
                     zoom:0.6;
-                    font-size:12px !important;
+                    font-size:12.5px !important;
                 }
         
                 table > thead > tr > th {
@@ -71,7 +71,7 @@
                 }
         
                 table > thead > tr > th {
-                    font-size:12px !important;
+                    font-size:12.5px !important;
                     font-weight: 800 !important;
                 }
         

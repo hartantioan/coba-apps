2454c7c6d8fa2f359b136201b635196567c4308b
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-26 17:54:27 +0700

    gas

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -25,7 +25,7 @@
     <script src="{{ url('app-assets/js/plugins.js?v=7') }}"></script>
     <script src="{{ url('app-assets/js/search.js?v=11') }}"></script>
     <script src="{{ url('app-assets/vendors/ckeditor/ckeditor.js') }}"></script>
-    <script src="{{ url('app-assets/js/custom/custom-script.js?v=146') }}"></script>
+    <script src="{{ url('app-assets/js/custom/custom-script.js?v=147') }}"></script>
     <script disable-devtool-auto src='{{ url('app-assets/js/custom/disabledevtools.js') }}' disable-menu='false'></script>
     
     <script src="{{ url('app-assets/js/custom/go-chart.js') }}"></script>

2d5b3a7425c79a33fee7ff217e14cd7c2a5a08d1
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-26 16:33:08 +0700

    Update ExportProductionBatchStock.php

--- app/Exports/ExportProductionBatchStock.php
+++ app/Exports/ExportProductionBatchStock.php
@@ -53,128 +53,45 @@ class ExportProductionBatchStock implements FromView,ShouldAutoSize
                 });
             }
         })
-        ->where('qty_final','>',0)
         ->whereHas('productionBatch')
         ->orderBy('date', 'desc')
-        ->get();
+        ->get();      
         
-      
-        
-        $previousId = null; // Initialize the previous ID variable
-        $cum_qty = 0;
         $cum_val = 0 ;
-        $array_filter=[];
-        $uom_unit = null;
-        $previousId = null;
         $array_last_item = [];
         $array_first_item = [];
         $all_total = 0;
+
         foreach($query_data as $row){
             $arr = $row->infoFg();
-
-            $priceNow = $arr['qty'] > 0 ? $arr['total'] / $arr['qty'] : 0;
-        
-            $all_total += round($arr['total'],2);
+            if(round($arr['qty'],3) > 0){
+                $priceNow = $arr['total'] / $arr['qty'];
             
-            $data_tempura = [
-                'item_id'      => $row->item->id,
-                'perlu'        => 0,
-                'plant' => $row->place->code,
-                'warehouse' => $row->warehouse->name,
-                'item' => $row->item->name,
-                'satuan' => $row->item->uomUnit->code,
-                'kode' => $row->item->code,
-                'area' => $row->area->name ?? '-',
-                'shading' => $row->itemShading->code ?? '-',
-                'production_batch' => $row->productionBatch()->exists() ? $row->productionBatch->code : '-',
-                'final'=>number_format($priceNow,2,',','.'),
-                'total'=>$perlu == 0 ? '-' : number_format($cum_val,2,',','.'),
-                'qty' => $perlu == 0 ? '-' : CustomHelper::formatConditionalQty($arr['qty']),
-                'date' =>  date('d/m/Y',strtotime($row->date)),
-                'document' => $row->lookable->code,
-                'cum_qty' => CustomHelper::formatConditionalQty($arr['qty']),
-                'cum_val' => number_format($arr['total'],2,',','.'),
-            ];
-            $array_filter[]=$data_tempura;
-            if ($row->item_id !== $previousId) {
-              
-                $query_first =
-                ItemCogs::where(function($query) use ( $row) {
-                    $query->where('item_id',$row->item_id)
-                    ->where('date', '<', $row->date);
-                    
-                    if($this->plant != 'all'){
-                        $query->whereHas('place',function($query){
-                            $query->where('id',$this->plant);
-                        });
-                    }
-                    if($this->warehouse != 'all'){
-                        $query->whereHas('warehouse',function($query){
-                            $query->where('id',$this->warehouse);
-                        });
-                    }
-                })
-                ->orderBy('date', 'desc') // Order by 'date' column in descending order
-                ->orderBy('id','desc')
-                ->first();
+                $all_total += round($arr['total'],2);
+                
+                $data_tempura = [
+                    'item_id'      => $row->item->id,
+                    'perlu'        => 0,
+                    'plant' => $row->place->code,
+                    'warehouse' => $row->warehouse->name,
+                    'item' => $row->item->name,
+                    'satuan' => $row->item->uomUnit->code,
+                    'kode' => $row->item->code,
+                    'area' => $row->area->name ?? '-',
+                    'shading' => $row->itemShading->code ?? '-',
+                    'production_batch' => $row->productionBatch()->exists() ? $row->productionBatch->code : '-',
+                    'final'=>number_format($priceNow,2,',','.'),
+                    'total'=>$perlu == 0 ? '-' : number_format($cum_val,2,',','.'),
+                    'qty' => $perlu == 0 ? '-' : CustomHelper::formatConditionalQty($arr['qty']),
+                    'date' =>  date('d/m/Y',strtotime($row->date)),
+                    'document' => $row->lookable->code,
+                    'cum_qty' => CustomHelper::formatConditionalQty($arr['qty']),
+                    'cum_val' => number_format($arr['total'],2,',','.'),
+                ];
 
-                if($query_first){
-                    $arrFirst = $query_first->infoFg();
-                    $array_last_item[] = [
-                        'perlu'        => 1,
-                        'item_id'      => $row->item->id,
-                        'id'           => $query_first->id ?? null, 
-                        'date'         => $query_first ? date('d/m/Y', strtotime($query_first->date)) : null,
-                        'last_nominal' => $query_first ? number_format($arrFirst['total'], 2, ',', '.') : 0,
-                        'item'         => $row->item->name,
-                        'satuan'       => $row->item->uomUnit->code,
-                        'area'         => $row->area->name ?? '-',
-                        'production_batch' => '-',
-                        'shading' => $row->shading->code ?? '-',
-                        'kode'         => $row->item->code,
-                        'last_qty'     => $query_first ? CustomHelper::formatConditionalQty($arrFirst['qty']) : 0,
-                    ];
-                }
+                $array_filter[]=$data_tempura;
             }
-            $previousId = $row->item_id;
-            
-            if($uom_unit ===null){
-                $uom_unit = $row->item->uomUnit->code;
-            }
-            
-            
         }
-      
-        
-        $combinedArray = [];
-
-        // Merge $array_filter into $combinedArray
-        foreach ($array_filter as $item) {
-            $combinedArray[] = $item;
-        }
-
-        // Merge $array_last_item into $combinedArray
-        foreach ($array_last_item as $item) {
-            $combinedArray[] = $item;
-        }
-
-        // Merge $array_first_item into $combinedArray
-        foreach ($array_first_item as $item) {
-            $combinedArray[] = $item;
-        }
-        usort($combinedArray, function ($a, $b) {
-            // First, sort by 'kode' in ascending order
-            $kodeComparison = strcmp($a['kode'], $b['kode']);
-            
-            if ($kodeComparison !== 0) {
-                return $kodeComparison;
-            }
-        
-            // If 'kode' is the same, prioritize 'perlu' in descending order
-            return $b['perlu'] - $a['perlu'];
-        });
-  
-        $combinedArray=$array_filter;
      
 
         activity()
@@ -184,10 +101,10 @@ class ExportProductionBatchStock implements FromView,ShouldAutoSize
             ->log('Export stock in rupiah data  .');
 
         return view('admin.exports.production_batch_stock', [
-            'data'          => $combinedArray,
+            'data'          => $array_filter,
             'latest'        => $array_last_item,
             'first'         => $array_first_item,
-            'perlu'         =>  $perlu,
+            'perlu'         => $perlu,
         ]);
     }
 }

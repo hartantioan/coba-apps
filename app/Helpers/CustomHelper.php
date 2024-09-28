2c9b79021578b7c82f591c8fe05df732b9489879
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-28 12:12:20 +0700

    Update CustomHelper.php

--- app/Helpers/CustomHelper.php
+++ app/Helpers/CustomHelper.php
@@ -7251,6 +7251,59 @@ class CustomHelper {
         }
 	}
 
+	public static function accumulateCogsForAll($date,$company_id,$place_id,$item_id){
+		$itemcogs2 = ItemCogs::where('date','>=',$date)->where('company_id',$company_id)->where('place_id',$place_id)->where('item_id',$item_id)->orderBy('date')->orderBy('id')->get();
+		$old_data2 = ItemCogs::where('date','<',$date)->where('company_id',$company_id)->where('place_id',$place_id)->where('item_id',$item_id)->orderByDesc('date')->orderByDesc('id')->first();
+
+		$total_final = 0;
+		$qty_final = 0;
+		$price_final = 0;
+		foreach($itemcogs2 as $key2 => $row){
+			if($key2 == 0){
+				if($old_data2){
+					if($row->type == 'IN'){
+						$total_final = $old_data2->total_final + $row->total_in;
+						$qty_final = $old_data2->qty_final + $row->qty_in;
+					}elseif($row->type == 'OUT'){
+						$total_final = $old_data2->total_final - $row->total_out;
+						$qty_final = $old_data2->qty_final - $row->qty_out;
+					}
+	
+					$price_final = $qty_final > 0 ? round($total_final / $qty_final,5) : 0;
+				}else{
+					if($row->type == 'IN'){
+						$total_final = $row->total_in;
+						$qty_final = $row->qty_in;
+					}elseif($row->type == 'OUT'){
+						$total_final = 0 - $row->total_out;
+						$qty_final = 0 - $row->qty_out;
+					}
+		
+					$price_final = $qty_final > 0 ? round($total_final / $qty_final,5) : 0;
+				}
+				$row->update([
+					'price_final'	=> $price_final,
+					'qty_final'		=> round($qty_final,3),
+					'total_final'	=> round($total_final,2),
+				]);
+			}else{
+				if($row->type == 'IN'){
+					$total_final += $row->total_in;
+					$qty_final += $row->qty_in;
+				}elseif($row->type == 'OUT'){
+					$total_final -= $row->total_out;
+					$qty_final -= $row->qty_out;
+				}
+				$price_final = $qty_final > 0 ? round($total_final / $qty_final,5) : 0;
+				$row->update([
+					'price_final'	=> $price_final,
+					'qty_final'		=> round($qty_final,3),
+					'total_final'	=> round($total_final,2),
+				]);
+			}
+		}
+	}
+
 	public static function splitBomArray($data){
 		$newarray = [];
 		$arritem = [];

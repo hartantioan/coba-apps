7aebbb8837d4c226b8a0acb3c876af94f5539337
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-09-30 13:25:22 +0700

    Update ExportMarketingRecapitulationCsv.php

--- app/Exports/ExportMarketingRecapitulationCsv.php
+++ app/Exports/ExportMarketingRecapitulationCsv.php
@@ -69,7 +69,8 @@ class ExportMarketingRecapitulationCsv implements FromCollection, WithTitle, Sho
         
         foreach($invoice as $key => $row){
             $arrTemp = explode('.',$row->tax_no);
-            $transactionCode = substr_count($arrTemp[0],'0') == 2 ? substr($arrTemp[0],0,3) : intval($arrTemp[0]);
+            /* $transactionCode = substr_count($arrTemp[0],'0') == 2 ? substr($arrTemp[0],0,3) : intval($arrTemp[0]); */
+            $transactionCode = $arrTemp[0];
             array_splice($arrTemp,0,1);
             $tax_no = implode('',$arrTemp);
             $month = date('n',strtotime($row->post_date));

9c42d63e69ed3882a854b31d67cf047273ab9002
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-10-02 04:46:11 +0700

    Update MenuController.php

--- app/Http/Controllers/Setting/MenuController.php
+++ app/Http/Controllers/Setting/MenuController.php
@@ -100,13 +100,13 @@ class MenuController extends Controller
             ResetCogsHelper::gas($startdate,1,1,$item->id,NULL,NULL,NULL);
         } */
 
-        /* $data = [
+        $data = [
             'title'     => 'Menu',
             'menu'      => Menu::whereNull('parent_id')->where('status','1')->oldest('order')->get(),
             'content'   => 'admin.setting.menu'
         ];
 
-        return view('admin.layouts.index', ['data' => $data]); */
+        return view('admin.layouts.index', ['data' => $data]);
 
         /* $data = MarketingOrderDeliveryProcess::whereHas('marketingOrderInvoice')->get();
 

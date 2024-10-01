aac7e51437a45e69034b15384e19bfcb2a39e2fc
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-10-02 06:03:38 +0700

    Update MenuController.php

--- app/Http/Controllers/Setting/MenuController.php
+++ app/Http/Controllers/Setting/MenuController.php
@@ -168,19 +168,19 @@ class MenuController extends Controller
             ResetCogsNew::dispatch($batch->post_date,1,$batch->place_id,$batch->item_id,$batch->area_id,$batch->item_shading_id,$batch->id);
         } */
 
-        $data = Item::where('item_group_id',49)->get();
+        /* $data = Item::where('item_group_id',49)->get();
 
         foreach($data as $item){
             ResetCogsNew::dispatch('2024-09-03',1,1,$item->id,NULL,NULL,NULL);
-        }
+        } */
 
-        /* $data = ProductionBatch::whereNotNull('lookable_type')->where('post_date','>=','2024-09-03')->whereHas('item',function($query){
+        $data = ProductionBatch::whereNotNull('lookable_type')->where('post_date','>=','2024-09-03')->whereHas('item',function($query){
             $query->where('item_group_id',7);
         })->get();
 
         foreach($data as $batch){
             ResetCogsNew::dispatch($batch->post_date,1,$batch->place_id,$batch->item_id,$batch->area_id,$batch->item_shading_id,$batch->id);
-        } */
+        }
 
         /* ResetCogsNew::dispatch('2024-09-30',1,1,4385,NULL,NULL,NULL); */
 

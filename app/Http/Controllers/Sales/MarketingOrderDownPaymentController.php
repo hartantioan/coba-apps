2951b3fc1c39ada945234835f679a007ab0deb29
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-10-01 12:39:36 +0700

    Update MarketingOrderDownPaymentController.php

--- app/Http/Controllers/Sales/MarketingOrderDownPaymentController.php
+++ app/Http/Controllers/Sales/MarketingOrderDownPaymentController.php
@@ -680,7 +680,7 @@ class MarketingOrderDownPaymentController extends Controller
            
             $pdf = PrintHelper::print($pr,'Print AR Down Payment','a4','portrait','admin.print.sales.order_down_payment_individual',$menuUser->mode);
             $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-            $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
+            /* $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0)); */
             
             $content = $pdf->download()->getOriginalContent();
             
@@ -714,9 +714,9 @@ class MarketingOrderDownPaymentController extends Controller
                 if($pr){
                     $pdf = PrintHelper::print($pr,'Print AR Down Payment','a4','portrait','admin.print.sales.order_down_payment_individual');
                     $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-                    $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $pr->printCounter()->count(), $font, 10, array(0,0,0));
+                    /* $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $pr->printCounter()->count(), $font, 10, array(0,0,0));
                     $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-                    $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+                    $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0)); */
                     $content = $pdf->download()->getOriginalContent();
                     $temp_pdf[]=$content;
                 }
@@ -791,9 +791,9 @@ class MarketingOrderDownPaymentController extends Controller
                         if($query){
                             $pdf = PrintHelper::print($query,'Print AR Down Payment','a4','portrait','admin.print.sales.order_down_payment_individual');
                             $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-                            $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $query->printCounter()->count(), $font, 10, array(0,0,0));
+                            /* $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $query->printCounter()->count(), $font, 10, array(0,0,0));
                             $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-                            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+                            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0)); */
                             $content = $pdf->download()->getOriginalContent();
                             $temp_pdf[]=$content;
                            
@@ -845,9 +845,9 @@ class MarketingOrderDownPaymentController extends Controller
                         if($query){
                             $pdf = PrintHelper::print($query,'Print AR Down Payment','a4','portrait','admin.print.sales.order_down_payment_individual');
                             $font = $pdf->getFontMetrics()->get_font("helvetica", "bold");
-                            $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $query->printCounter()->count(), $font, 10, array(0,0,0));
+                            /* $pdf->getCanvas()->page_text(495, 340, "Jumlah Print, ". $query->printCounter()->count(), $font, 10, array(0,0,0));
                             $pdf->getCanvas()->page_text(505, 350, "PAGE: {PAGE_NUM} of {PAGE_COUNT}", $font, 10, array(0,0,0));
-                            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0));
+                            $pdf->getCanvas()->page_text(422, 360, "Print Date ". $formattedDate, $font, 10, array(0,0,0)); */
                             $content = $pdf->download()->getOriginalContent();
                             $temp_pdf[]=$content;
                            

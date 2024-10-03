14388ae4d16eb4a00e2e177852c2ad3a0e0b0196
Yustinus Windy <54971881+windy1989@users.noreply.github.com>
2024-10-03 11:42:57 +0700

    Update ProductionFgReceiveController.php

--- app/Http/Controllers/Production/ProductionFgReceiveController.php
+++ app/Http/Controllers/Production/ProductionFgReceiveController.php
@@ -366,7 +366,7 @@ class ProductionFgReceiveController extends Controller
                 foreach($arrBatch as $key => $row){
                     $pb = ProductionBatch::find($row);
                     if($pb){
-                        if($request->post_date >= $pb->lookable->productionReceive->post_date){
+                        if($request->post_date >= $pb->lookable->parent->post_date){
                             if($arrBatchQty[$key] > $pb->qty){
                                 $arrBatchError[] = 'Terdapat batch melebihi pemakaian stock : '.CustomHelper::formatConditionalQty($pb->qty).' sedangkan pemakaian : '.CustomHelper::formatConditionalQty($arrBatchQty[$key]).'.';
                                 $passedBatchUsed = false;

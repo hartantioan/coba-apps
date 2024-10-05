eee376250a5387191aeb6f4d5f0fc1bdb1af4ec3
Yustinus Windy <windykuro@gmail.com>
2024-10-05 22:48:58 +0700

    Update history_employee_receivable.blade.php

--- resources/views/admin/exports/history_employee_receivable.blade.php
+++ resources/views/admin/exports/history_employee_receivable.blade.php
@@ -29,7 +29,7 @@
                 <td style="background-color:red;color:white;">Keterangan</td>
                 <td style="background-color:red;color:white;">Nominal</td>
             </tr>
-            @if (count($row['details'] > 0))
+            @if (count($row['details']) > 0))
                 @foreach($row['details'] as $rowdetail)
                     <tr>
                         <td></td>

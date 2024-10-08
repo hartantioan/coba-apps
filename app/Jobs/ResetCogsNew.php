96cd3633a2cc77d1a079da309de0268a54e57c50
Yustinus Windy <windykuro@gmail.com>
2024-10-08 21:52:59 +0700

    Update ResetCogsNew.php

--- app/Jobs/ResetCogsNew.php
+++ app/Jobs/ResetCogsNew.php
@@ -598,7 +598,7 @@ class ResetCogsNew implements ShouldQueue/* , ShouldBeUnique */
         })->get();
 
         $tempgiprice = 0;
-        dd($totalBefore);
+        info($totalBefore);
         foreach($goodissue as $row){
             $price = $qtyBefore > 0 ? $totalBefore / $qtyBefore : 0;
            /*  if($tempgiprice > 0){

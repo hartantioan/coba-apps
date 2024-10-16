a7b7b83156f479d733b2916030a09b9759a5517f
Yustinus Windy <windykuro@gmail.com>
2024-10-16 13:02:55 +0700

    Update MenuController.php

--- app/Http/Controllers/Setting/MenuController.php
+++ app/Http/Controllers/Setting/MenuController.php
@@ -101,13 +101,19 @@ class MenuController extends Controller
             ResetCogsHelper::gas($startdate,1,1,$item->id,NULL,NULL,NULL);
         } */
 
-        $data = [
+        /* $data = [
             'title'     => 'Menu',
             'menu'      => Menu::whereNull('parent_id')->where('status','1')->oldest('order')->get(),
             'content'   => 'admin.setting.menu'
         ];
 
-        return view('admin.layouts.index', ['data' => $data]);
+        return view('admin.layouts.index', ['data' => $data]); */
+
+        $data = ProductionFgReceive::whereIn('status',['2','3'])->where('post_date','>=','2024-09-01')->where('post_date','<=','2024-09-30')->get();
+
+        foreach($data as $row){
+            $data->recalculate();
+        }
 
         /* $user = User::where('status','1')->where('type','1')->get();
 

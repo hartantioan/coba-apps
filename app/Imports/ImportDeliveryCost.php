f6dc4b1a7a0e75e6fc22c8bc355a4123ecb964c6
hartantioan <129711861+hartantioan@users.noreply.github.com>
2024-10-18 09:30:25 +0700

    Update ImportDeliveryCost.php

--- app/Imports/ImportDeliveryCost.php
+++ app/Imports/ImportDeliveryCost.php
@@ -30,7 +30,7 @@ class ImportDeliveryCost implements WithMultipleSheets
         ->causedBy(session('bo_id'))
         ->withProperties('')
         ->log('Add / edit from excel Delivery cost data.');
-               
+
         return [
             0 => new handleDC(),
         ];
@@ -47,21 +47,24 @@ class handleDC implements OnEachRow, WithHeadingRow
         DB::beginTransaction();
         try {
             if (isset($row['code']) && $row['code']) {
-               
+
 
                 $code = $row['code'];$name = $row['name'];$tonnage = $row['kg_price'];$ritage = $row['ritage_price'];$tonnage_weight = $row['tonage_weight'];
                 $account = User::where('employee_no', explode('#', $row['supplier_ekspedisi'])[0])->first();
-                
+                if(isset($row['code']) && $row['code']){
+                    $check = DeliveryCost::where('code',$row['code'])->first();
+
+                }
                 $city = str_replace(',', '.', explode('#', $row['from_city'])[0]);
-                $city_id_from = Region::where('code',$city)->first()->id; 
+                $city_id_from = Region::where('code',$city)->first()->id;
                 $from_sub_district = str_replace(',', '.', explode('#', $row['from_subdistrict'])[0]);
                 $sub_district_id_from = Region::where('code',$from_sub_district)->first()->id;
 
                 $tocity  = str_replace(',', '.', explode('#', $row['to_city'])[0]);
-                $city_id_to = Region::where('code',$tocity)->first()->id; 
+                $city_id_to = Region::where('code',$tocity)->first()->id;
                 $to_sub_district  = str_replace(',', '.', explode('#', $row['to_subdistrict'])[0]);
                 $sub_district_id_to = Region::where('code',$to_sub_district)->first()->id;
-                
+
                 $transport = Transportation::where('code', explode('#', $row['transport'])[0])->first();
                 if(!$account && $this->error ==null){
                     $this->error = "Ekspedisi dan Supplier.";
@@ -91,7 +94,26 @@ class handleDC implements OnEachRow, WithHeadingRow
                 $dateFormatted1 = $dateTime1->format('Y/m/d');
                 $dateTime2 = DateTime::createFromFormat('U', ($row['valid_to'] - 25569) * 86400);
                 $dateFormatted2 = $dateTime2->format('Y/m/d');
-              
+
+                if($check){
+                    $check->code = $row['code'];
+                    $check->user_id = session('bo_id');
+                    $check->account_id = $account->id;
+                    $check->name = $row['name'];
+                    $check->valid_from = $dateFormatted1;
+                    $check->valid_to = $dateFormatted2;
+                    $check->from_city_id = $city_id_from;
+                    $check->from_subdistrict_id = $sub_district_id_from;
+                    $check->to_city_id = $city_id_to;
+                    $check->to_subdistrict_id = $sub_district_id_to;
+                    $check->transportation_id = $transport->id;
+                    $check->tonnage = $tonnage;
+                    $check->ritage = $ritage;
+                    $check->qty_tonnage = $tonnage_weight;
+                    $check->status =  1;
+
+                    $check->save();
+                }else{
                     $query = DeliveryCost::create([
                         'code' => $row['code'],
                         'user_id' => session('bo_id'),
@@ -109,13 +131,15 @@ class handleDC implements OnEachRow, WithHeadingRow
                         'qty_tonnage' => $tonnage_weight,
                         'status'=> 1
                     ]);
-                
-                    
+                }
+
+
+
             }else{
                 DB::rollback();
                 $sheet='Header';
                 throw new RowImportException('baris kode kosong', $row->getIndex(),$this->error,$sheet);
-            } 
+            }
             DB::commit();
         }catch (\Exception $e) {
             DB::rollback();
@@ -123,7 +147,7 @@ class handleDC implements OnEachRow, WithHeadingRow
             throw new RowImportException($e->getMessage(), $row->getIndex(),$this->error,$sheet);
         }
     }
-    
+
     public function startRow(): int
     {
         return 2; // If you want to skip the first row (heading row)

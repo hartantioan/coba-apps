c7ca6d4cfbb6b47d19128595441b7996b83e96b8
Yustinus Windy <windykuro@gmail.com>
2024-10-19 10:16:13 +0700

    tgl invoice tidak boleh kurang dari tgl sj

--- app/Http/Controllers/Sales/MarketingOrderInvoiceController.php
+++ app/Http/Controllers/Sales/MarketingOrderInvoiceController.php
@@ -394,6 +394,16 @@ class MarketingOrderInvoiceController extends Controller
                     }
                 } */
 
+                $modp = MarketingOrderDeliveryProcess::find($request->marketing_order_delivery_process_id);
+
+                if($modp){
+                    if($request->post_date < $modp->post_date){
+                        return response()->json([
+                            'status'  => 500,
+                            'message' => 'Mohon maaf! Tanggal invoice tidak boleh kurang dari tanggal post date SJ',
+                        ]);
+                    }
+                }
 
                 if($request->temp){
 

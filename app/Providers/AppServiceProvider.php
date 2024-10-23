aca4f100b23f1a094bf15d52f98409bb995753b7
Yustinus Windy <windykuro@gmail.com>
2024-10-23 16:27:23 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         date_default_timezone_set('Asia/Jakarta');
         Relation::morphMap([
             'purchase_requests'                     => 'App\Models\PurchaseRequest',

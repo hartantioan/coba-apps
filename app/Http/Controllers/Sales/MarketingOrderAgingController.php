2dbbf6d4c8fd10e74f7cdf5770e9d17ebae5b78c
Yustinus Windy <windykuro@gmail.com>
2024-10-24 15:20:08 +0700

    Update MarketingOrderAgingController.php

--- app/Http/Controllers/Sales/MarketingOrderAgingController.php
+++ app/Http/Controllers/Sales/MarketingOrderAgingController.php
@@ -311,7 +311,7 @@ class MarketingOrderAgingController extends Controller
                 <th rowspan="2" class="center-align">No.</th>
                 <th rowspan="2" class="center-align" style="min-width:250px !important;">Supplier</th>
                 <th rowspan="2" class="center-align" style="min-width:250px !important;">Invoice</th>
-                <th rowspan="2" class="center-align" style="min-width:175px !important;">Nominal</th>
+                <th rowspan="2" class="center-align" style="min-width:175px !important;">Total Piutang</th>
                 <th colspan="'.$countPeriod.'">Nominal Jatuh Tempo (Dari Tgl. Posting dan Tgl. Jatuh Tempo)</th>
                 
             </tr>

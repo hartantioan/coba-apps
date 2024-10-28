9e6dbf8a2f61d067637051c130149c38604e0db9
Yustinus Windy <windykuro@gmail.com>
2024-10-28 15:35:09 +0700

    Update order_memo.blade.php

--- resources/views/admin/sales/order_memo.blade.php
+++ resources/views/admin/sales/order_memo.blade.php
@@ -1607,10 +1607,10 @@
                             beforeSend: function() {
                                 $('#validation_alert').hide();
                                 $('#validation_alert').html('');
-                                loadingOpen('.modal-content');
+                                loadingOpen('#modal1');
                             },
                             success: function(response) {
-                                loadingClose('.modal-content');
+                                loadingClose('#modal1');
                                 $('input').css('border', 'none');
                                 $('input').css('border-bottom', '0.5px solid black');
                                 if(response.status == 200) {
@@ -1654,7 +1654,7 @@
                             },
                             error: function() {
                                 $('.modal-content').scrollTop(0);
-                                loadingClose('.modal-content');
+                                loadingClose('#modal1');
                                 swal({
                                     title: 'Ups!',
                                     text: 'Check your internet connection.',

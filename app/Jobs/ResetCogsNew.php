82a8d9557c5abf10e4365d671f78eed8f5cfed80
Yustinus Windy <windykuro@gmail.com>
2024-10-30 16:11:03 +0700

    Update ResetCogsNew.php

--- app/Jobs/ResetCogsNew.php
+++ app/Jobs/ResetCogsNew.php
@@ -1180,7 +1180,7 @@ class ResetCogsNew implements ShouldQueue/* , ShouldBeUnique */
       $itemstock = ItemStock::where('item_id',$item_id)->where('place_id',$place_id)->where('warehouse_id',$item->warehouse())->where('area_id',$area_id)->where('item_shading_id',$item_shading_id)->where('production_batch_id',$production_batch_id)->first();
       if($itemstock){
           $itemstock->update([
-              'qty'   => $qtyBefore,
+              'qty'   => $itemstock->stockByDate(date('Y-m-d')),
           ]);
       }
     }

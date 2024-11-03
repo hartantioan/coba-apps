d733b9c1508bb7e4d89770ec128f340f75bad2da
Andrias Lukman <andribakcang@gmail.com>
2024-11-03 11:27:46 +0700

    Update report_marketing_global.blade.php

--- resources/views/admin/mail/report_marketing_global.blade.php
+++ resources/views/admin/mail/report_marketing_global.blade.php
@@ -127,14 +127,11 @@
         <td style="font-size:12px;" align="right">{{number_format($row->sisamod,0,",",".")}}</td>
         <td style="font-size:12px;" align="right">{{number_format($row->sjm,0,",",".")}}</td>
         @endif
-        @if ($row->tipe=='HT' || $row->tipe=='GLAZED')
-        <td style="font-size:12px;" align="right">-</td>
-
-        @else
+       
 
         <td style="font-size:12px;" align="right">{{number_format($row->asp,0,",",".")}}</td>
 
-        @endif
+       
     </tr>
     @php
     $i++;

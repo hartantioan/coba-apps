406b79136df4c305714a49cfe20ab416d504633c
Andrias Lukman <andribakcang@gmail.com>
2024-11-03 11:28:22 +0700

    Update MailReportMarketingGlobal.php

--- app/Console/Commands/MailReportMarketingGlobal.php
+++ app/Console/Commands/MailReportMarketingGlobal.php
@@ -40,8 +40,8 @@ class MailReportMarketingGlobal extends Command
 	 */
 	public function handle()
 	{
-		//$recipient = ['henrianto@superior.co.id', 'hendra@superior.co.id', 'andrew@superior.co.id', 'haidong@superiorporcelain.co.id', 'billylaw@superior.co.id'];
-		$recipient = ['edp@superior.co.id'];
+		$recipient = ['henrianto@superior.co.id', 'hendra@superior.co.id', 'andrew@superior.co.id', 'haidong@superiorporcelain.co.id', 'billylaw@superior.co.id'];
+		//$recipient = ['edp@superior.co.id'];
 		//  $akun = MarketingOrderInvoice::whereIn('status',[2])->distinct('account_id')->get('account_id');
 
 		// foreach ($akun as $pangsit) {

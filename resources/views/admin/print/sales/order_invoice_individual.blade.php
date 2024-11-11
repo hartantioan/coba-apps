40bbe38b7e7fd6544bc57f6fc44c9accc760c616
hartantioan <129711861+hartantioan@users.noreply.github.com>
2024-11-11 12:00:30 +0700

    Update order_invoice_individual.blade.php

--- resources/views/admin/print/sales/order_invoice_individual.blade.php
+++ resources/views/admin/print/sales/order_invoice_individual.blade.php
@@ -17,7 +17,7 @@
             margin-left:-5px;
             margin-right:-5px;
             }
-            
+
             .column1 {
             float: left;
             width: 50%;
@@ -39,41 +39,41 @@
 
             td {
                 vertical-align:top !important;
-            }            
+            }
 
             @media only screen and (max-width : 768px) {
                 .invoice-print-area {
                     zoom:0.4;
                 }
             }
-        
+
             @media only screen and (max-width : 992px) {
                 .invoice-print-area {
                     zoom:0.6;
                     font-size:9px !important;
                 }
-        
+
                 table > thead > tr > th {
-                    
+
                     font-size:11px !important;
                     font-weight: 800 !important;
                 }
                 td{
                     font-size:1em !important;
-              
+
                 }
             }
-        
+
             @media print {
                 .invoice-print-area {
                     font-size:11px !important;
                 }
-        
+
                 table > thead > tr > th {
                     font-size:13px !important;
                     font-weight: 800 !important;
                 }
-        
+
                 td {
                     border:none !important;
                     border-bottom: none;
@@ -81,33 +81,33 @@
                     padding: 1px !important;
                     vertical-align:top !important;
                 }
-        
+
                 body {
                     background-color:white !important;
                     zoom:0.8;
                 }
-                
+
                 .modal {
                     background-color:white !important;
                 }
-        
+
                 .card {
                     background-color:white !important;
                     padding:25px !important;
                 }
-        
+
                 .invoice-print-area {
                     color: #000000 !important;
                 }
-        
+
                 .invoice-subtotal {
                     color: #000000 !important;
                 }
-        
+
                 .invoice-info {
-                    font-size:10px !important;
+                    font-size:9px !important;
                 }
-        
+
                 .modal {
                     position: absolute;
                     left: 0;
@@ -118,22 +118,22 @@
                     overflow: visible !important;
                     min-width:100% !important;
                 }
-                
+
                 .modal-content {
                     visibility: visible !important;
                     overflow: visible !important;
                     padding: 0px !important;
                 }
-        
+
                 .modal-footer {
                     display:none !important;
                 }
-        
+
                 .row .col {
                     padding:0px !important;
                 }
             }
-            
+
             .invoice-product-details{
                 /* border:1px solid black; */
                 min-height: auto;
@@ -141,9 +141,9 @@
 
             @page { margin: 4em 2em 4em 1em; }
             header { position: fixed; top: -30px; left: 0px; right: 0px; height: 100px; margin-bottom: 10em }
-                
-        
-           
+
+
+
         </style>
     </head>
     <body style="margin-top: 20px">
@@ -151,13 +151,77 @@
             <table border="0" width="100%">
                 <tr>
                     <td width="33%">
-                        
+
                     </td>
-                    <td width="33%" align="center">
+                    <td width="33%" align="center" >
                         <h5 style="margin-top:35px;">{{ $data->code }}</h5>
                     </td>
                     <td width="33%">
-                        
+
+                    </td>
+                </tr>
+            </table>
+            <table border="0" width="100%" style="margin-top:-13px;">
+                <tr>
+                    <td width="60%" class="left-align">
+                        <table border="0" width="100%">
+                            <tr>
+                                <td width="20%">
+
+                                </td>
+
+                                <td width="80%" style="font-size: 9px !important">
+                                    <div style="min-height:40px">
+                                    {{ $data->userData->title }}
+                                    <br>
+                                    {{ $data->userData->address }}
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td width="40%" class="left-align" style="padding-top: 0px">
+                        <table border="0" width="100%" >
+                            <tr>
+                                <td width="50%">
+
+                                </td>
+
+                                <td width="50%" style="font-size: 9px !important">
+                                    {{ date('d/m/Y',strtotime($data->post_date)) }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+
+                                </td>
+
+                                <td style="font-size: 9px !important">
+                                    {{ date('d/m/Y',strtotime($data->due_date)) }}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+
+            <table border="0" width="100%">
+                <tr>
+                    <td width="11%">
+
+                    </td>
+
+                    <td width="80%" style="font-size: 9px !important">
+                        {{ $data->marketingOrderDeliveryProcess()->exists() ? $data->marketingOrderDeliveryProcess->marketingOrderDelivery->code : '-' }}
+                    </td>
+                </tr>
+                <tr>
+                    <td width="11%">
+
+                    </td>
+
+                    <td width="80%" style="font-size: 9px !important">
+                        {{ $data->marketingOrderDeliveryProcess()->exists() ? $data->marketingOrderDeliveryProcess->code : '-' }}
                     </td>
                 </tr>
             </table>
@@ -166,15 +230,15 @@
             <div class="card" style="margin-top:15px;">
                 <div class="card-content invoice-print-area">
                     <!-- header section -->
-                    <table border="0" width="100%">
+                    {{-- <table border="0" width="100%">
                         <tr>
                             <td width="60%" class="left-align">
                                 <table border="0" width="100%">
                                     <tr>
                                         <td width="20%">
-                                           
+
                                         </td>
-                                        
+
                                         <td width="80%">
                                             <div style="min-height:40px">
                                             {{ $data->userData->title }}
@@ -189,18 +253,18 @@
                                 <table border="0" width="100%" >
                                     <tr>
                                         <td width="50%">
-                                           
+
                                         </td>
-                                        
+
                                         <td width="50%">
                                             {{ date('d/m/Y',strtotime($data->post_date)) }}
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>
-                                            
+
                                         </td>
-                                        
+
                                         <td>
                                             {{ date('d/m/Y',strtotime($data->due_date)) }}
                                         </td>
@@ -213,33 +277,34 @@
                     <table border="0" width="100%">
                         <tr>
                             <td width="11%">
-                                
+
                             </td>
-                            
+
                             <td width="80%">
                                 {{ $data->marketingOrderDeliveryProcess()->exists() ? $data->marketingOrderDeliveryProcess->marketingOrderDelivery->code : '-' }}
                             </td>
                         </tr>
                         <tr>
                             <td width="11%">
-                                
+
                             </td>
-                            
+
                             <td width="80%">
                                 {{ $data->marketingOrderDeliveryProcess()->exists() ? $data->marketingOrderDeliveryProcess->code : '-' }}
                             </td>
                         </tr>
-                    </table>
+                    </table> --}}
                     @php
                         $freeAreaTax = $data->marketingOrderDeliveryProcess()->exists() ? ($data->marketingOrderDeliveryProcess->marketingOrderDelivery->getMaxTaxType() == '2' ? '18' : '') : '';
                     @endphp
-                    <div class="invoice-product-details mt-2" style="padding-top:40px;padding-left:10px;padding-right:47px">
+                    <div class="invoice-product-details mt-2" style="padding-top:128px;padding-left:18px;padding-right:40px">
                         <table style="border-collapse:collapse;border:none;" width="100%">
                             <thead>
-                                
+
                             </thead>
                             <tbody>
                                 @foreach($data->marketingOrderInvoiceDeliveryProcessDetail as $key => $row)
+
                                 @php
                                     $boxQty = '';
                                     if($row->lookable->isPallet()){
@@ -262,11 +327,14 @@
                                     <td style="width:5% !important" align="right">{{ number_format($row->lookable->marketingOrderDeliveryDetail->marketingOrderDetail->discount_3,2,',','.') }}</td>
 
                                     <td style="width:20% !important" align="right">{{ number_format($row->total,2,',','.') }}</td>
-                                    
+
                                 </tr>
-                                
+
                                 @endforeach
                                 @foreach($data->marketingOrderInvoiceDeliveryDetail as $key => $row)
+                                    @if($key > 0 && $key % 6 == 0)
+                                        <div style="page-break-before: always;margin-top:140px;"></div>
+                                    @endif
                                 @php
                                     $boxQty = '';
                                     if($row->lookable->isPallet()){
@@ -289,9 +357,9 @@
                                     <td style="width:5% !important" align="right">{{ number_format($row->lookable->marketingOrderDetail->discount_3,2,',','.') }}</td>
 
                                     <td style="width:20% !important" align="right">{{ number_format($row->total,2,',','.') }}</td>
-                                    
+
                                 </tr>
-                                
+
                                 @endforeach
                                 @foreach($data->marketingOrderInvoiceDetailManual as $key => $row)
                                 <tr>
@@ -310,7 +378,7 @@
                             </tbody>
                         </table>
                     </div>
-                    
+
                     <!-- invoice subtotal -->
                     <div class="invoice-subtotal break-row" style="position: fixed;bottom: 73px; left: 0px; right: 45px;">
                         <div class="row">
@@ -321,7 +389,7 @@
                                     <td>
                                         <i>{{ CustomHelper::terbilangWithKoma($data->grandtotal) }} Rupiah
                                     </td>
-                                    
+
                                 </tr>
                             </table>
                         </div>
@@ -331,7 +399,7 @@
                                     <td align="right"></td>
                                     <td align="right" align="right" style="padding-top: 0px;padding-bottom:0px">{{ number_format($data->subtotal,2,',','.') }}</td>
                                 </tr>
-                                <tr style="margin-top: -10px !important">
+                                <tr style="margin-top: -9px !important">
                                     <td align="right"></td>
                                     <td align="right" align="right" style="padding-top: 0px;padding-bottom:0px">{{ number_format($data->downpayment,2,',','.') }}</td>
                                 </tr>
@@ -352,38 +420,9 @@
                         </div>
                     </div>
 
-                    {{-- @if($data->marketingOrderInvoiceDownPayment()->exists())
-                    <div class="invoice-product-details break-row" style="overflow:auto;margin-top:25px;">
-                        <div align="center">Downpayment Terpakai</div>
-                        <table border="1" style="border-collapse:collapse" width="100%">
-                            <thead>
-                                <tr>
-                                    <th>{{ __('translations.no') }}.</th>
-                                    <th>Dokumen</th>
-                                    <th>{{ __('translations.note') }}</th>
-                                    <th>{{ __('translations.total') }}</th>
-                                    <th>{{ __('translations.tax') }}</th>
-                                    <th>{{ __('translations.grandtotal') }}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($data->marketingOrderInvoiceDownPayment as $key => $row)
-                                <tr>
-                                    <td align="center">{{ ($key + 1) }}</td>
-                                    <td align="center">{{ $row->lookable->code }}</td>
-                                    <td>{{ $row->note }}</td>
-                                    <td align="right">{{ number_format($row->total,2,',','.') }}</td>
-                                    <td align="right">{{ number_format($row->tax,2,',','.') }}</td>
-                                    <td align="right">{{ number_format($row->grandtotal,2,',','.') }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    @endif --}}
                 </div>
             </div>
         </main>
-       
+
     </body>
 </html>

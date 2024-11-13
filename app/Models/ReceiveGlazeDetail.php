79325fb60ee3787cc51bf5a9c82b67bc02abed70
hartantioan <129711861+hartantioan@users.noreply.github.com>
2024-11-13 14:09:57 +0700

    Update ReceiveGlazeDetail.php

--- app/Models/ReceiveGlazeDetail.php
+++ app/Models/ReceiveGlazeDetail.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class ReceiveGlazeDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, Notifiable;
 
+    protected $table = 'receive_glaze_details';
+    protected $primaryKey = 'id';
+    protected $dates = ['deleted_at'];
     protected $fillable = [
         'receive_glaze_id', 'issue_glaze_id', 'qty'
     ];

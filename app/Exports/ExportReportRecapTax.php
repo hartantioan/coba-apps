1fb756ddefa8deee6882e658742d7b6b8a0d0685
Andrias Lukman <andribakcang@gmail.com>
2024-11-15 11:15:30 +0700

    Update ExportReportRecapTax.php

--- app/Exports/ExportReportRecapTax.php
+++ app/Exports/ExportReportRecapTax.php
@@ -73,84 +73,85 @@ class ExportReportRecapTax implements FromCollection, WithTitle, WithHeadings, S
 
             $freeAreaTax = $row->marketingOrderDeliveryProcess()->exists() ? ($row->marketingOrderDeliveryProcess->marketingOrderDelivery->getMaxTaxType() == '2' ? '18' : '') : '';
             foreach ($row->marketingOrderInvoiceDetail as $keyd => $row_detail) {
+                if ($row_detail->lookable_type == 'marketing_order_delivery_details' || $row_detail->lookable_type == 'marketing_order_delivery_process_details') {
+                    $dpp_discount_detail = 0;
+                    $dpp_total_detail = 0;
+                    $dpp_fp_detail = 0;
+                    $ppn_fp_detail = 0;
+                    $price_dpp_detail = 0;
+                    $total_detail = 0;
+                    $price_satuan = 0;
 
-                $dpp_discount_detail = 0;
-                $dpp_total_detail = 0;
-                $dpp_fp_detail = 0;
-                $ppn_fp_detail = 0;
-                $price_dpp_detail = 0;
-                $total_detail = 0;
-                $price_satuan = 0;
+                    $percentTax = 1;
 
-                $percentTax = 1;
+                    $jumlah_barang = 0;
 
-                $jumlah_barang = 0;
+                    if ($row_detail->getMarketingOrder()) {
 
-                if ($row_detail->getMarketingOrder()) {
+                        $boxQty = '';
+                        if ($row_detail->lookable->isPallet()) {
 
-                    $boxQty = '';
-                    if ($row_detail->lookable->isPallet()) {
+                            $boxQty = ' ( ' . CustomHelper::formatConditionalQty($row_detail->qty * $row_detail->getBoxConversion()) . ' BOX )';
+                        }
+                        $hscode = '';
+                        if ($freeAreaTax) {
+                            $hscode = ' ' . $row_detail->getHSCode();
+                        }
 
-                        $boxQty = ' ( ' . CustomHelper::formatConditionalQty($row_detail->qty * $row_detail->getBoxConversion()) . ' BOX )';
+                        if ($row_detail->is_include_tax == 1) {
+                            $percentTax = ($row_detail->getMarketingOrder->percent_tax + 100) / 100;
+                        }
+
+                        //  $price_satuan = $row_detail->getMarketingOrder->priceWTax();
+                        $price_satuan = round($row_detail->price / $percentTax, 2);
+                        $jumlah_barang = $row_detail->getMarketingOrder->qty_uom;
+
+                        $dpp_discount_detail = round($row_detail->getMarketingOrder->price / $percentTax - $row_detail->getMarketingOrder->price_after_discount / $percentTax, 2);
+                        $dpp_discount_total += $dpp_discount_detail;
+
+                        $dpp_total_detail = round($row_detail->getMarketingOrder->price_after_discount *  $row_detail->getMarketingOrder->qty_uom / $percentTax, 2);
+
+                        $dpp_total += $dpp_total_detail;
+
+                        // $total_detail = round($row_detail->getMarketingOrder->total / $percentTax, 2);
+                        $total_detail = round($row_detail->total, 2);
+                        $total_all += $total_detail;
+
+
+                        $ppn_fp_detail = $row_detail->tax;
+                        $ppn_fp_total += $ppn_fp_detail;
+
+                        // $price_dpp_detail = round((($row_detail->getMarketingOrder->price_after_discount * $row_detail->getMarketingOrder->qty_uom) - $dpp_discount_detail) / $percentTax, 2);
+                        $price_dpp_detail = round((($price_satuan - $dpp_discount_detail)), 2);
+                        $price_dpp_total += $price_dpp_detail;
                     }
-                    $hscode = '';
-                    if ($freeAreaTax) {
-                        $hscode = ' ' . $row_detail->getHSCode();
-                    }
-
-                    if ($row_detail->is_include_tax == 1) {
-                        $percentTax = ($row_detail->getMarketingOrder->percent_tax + 100) / 100;
-                    }
-
-                    //  $price_satuan = $row_detail->getMarketingOrder->priceWTax();
-                    $price_satuan = round($row_detail->price / $percentTax, 2);
-                    $jumlah_barang = $row_detail->getMarketingOrder->qty_uom;
-
-                    $dpp_discount_detail = round($row_detail->getMarketingOrder->price / $percentTax - $row_detail->getMarketingOrder->price_after_discount / $percentTax, 2);
-                    $dpp_discount_total += $dpp_discount_detail;
-
-                    $dpp_total_detail = round($row_detail->getMarketingOrder->price_after_discount *  $row_detail->getMarketingOrder->qty_uom / $percentTax, 2);
-                  
-                    $dpp_total += $dpp_total_detail;
-
-                    // $total_detail = round($row_detail->getMarketingOrder->total / $percentTax, 2);
-                    $total_detail = round($row_detail->total, 2);
-                    $total_all += $total_detail;
-
-
-                    $ppn_fp_detail = $row_detail->tax;
-                    $ppn_fp_total += $ppn_fp_detail;
-
-                    // $price_dpp_detail = round((($row_detail->getMarketingOrder->price_after_discount * $row_detail->getMarketingOrder->qty_uom) - $dpp_discount_detail) / $percentTax, 2);
-                    $price_dpp_detail = round((($price_satuan - $dpp_discount_detail)), 2);
-                    $price_dpp_total += $price_dpp_detail;
+                    $detail[] = [
+                        'No Urut' => '',
+                        'Jenis Dokumen' => $row->invoiceType(),
+                        'Tipe Penjualan' => $row->soType(),
+                        'No Seri Pajak' => $row->tax_no,
+                        'No Dokumen' => $row->code,
+                        'Tgl Dokumen' => date('d/m/Y', strtotime($row->post_date)),
+                        'Nama NPWP' => $row->userData->user->name,
+                        'No NPWP' => $row->getNpwp(),
+                        'Alamat NPWP' => $row->userData->address,
+                        'Nama Barang' => $row_detail->getItem() . $boxQty . $hscode,
+                        'DPP Harga Satuan' => $price_satuan,
+                        'Jumlah Barang (Qty)' => $jumlah_barang,
+                        '% Diskon' => $row_detail->getMarketingOrder() ? $row_detail->getMarketingOrder->percent_discount_1 : '',
+                        '% Diskon 2' => $row_detail->getMarketingOrder() ? $row_detail->getMarketingOrder->percent_discount_2 : '',
+                        'Diskon 3' => $row_detail->getMarketingOrder() ? $row_detail->getMarketingOrder->discount_3 : '',
+                        'DPP Diskon' => $dpp_discount_detail,
+                        'Total Harga Barang (DPP)' => $price_dpp_detail,
+                        'Uang Muka (DP)' => '',
+                        'Total' =>  $total_detail,
+                        'DPP FP' => $total_detail,
+                        'PPN FP' => floor($ppn_fp_detail),
+                        'Status Cancel' => '',
+                        'Tipe Pembayaran' => '',
+                        'Pembuat' => '',
+                    ];
                 }
-                $detail[] = [
-                    'No Urut' => '',
-                    'Jenis Dokumen' => $row->invoiceType(),
-                    'Tipe Penjualan' => $row->soType(),
-                    'No Seri Pajak' => $row->tax_no,
-                    'No Dokumen' => $row->code,
-                    'Tgl Dokumen' => date('d/m/Y', strtotime($row->post_date)),
-                    'Nama NPWP' => $row->userData->user->name,
-                    'No NPWP' => $row->getNpwp(),
-                    'Alamat NPWP' => $row->userData->address,
-                    'Nama Barang' => $row_detail->getItem() . $boxQty . $hscode,
-                    'DPP Harga Satuan' => $price_satuan,
-                    'Jumlah Barang (Qty)' => $jumlah_barang,
-                    '% Diskon' => $row_detail->getMarketingOrder() ? $row_detail->getMarketingOrder->percent_discount_1 : '',
-                    '% Diskon 2' => $row_detail->getMarketingOrder() ? $row_detail->getMarketingOrder->percent_discount_2 : '',
-                    'Diskon 3' => $row_detail->getMarketingOrder() ? $row_detail->getMarketingOrder->discount_3 : '',
-                    'DPP Diskon' => $dpp_discount_detail,
-                    'Total Harga Barang (DPP)' => $price_dpp_detail,
-                    'Uang Muka (DP)' => '',
-                    'Total' =>  $total_detail,
-                    'DPP FP' => $total_detail,
-                    'PPN FP' => floor($ppn_fp_detail),
-                    'Status Cancel' => '',
-                    'Tipe Pembayaran' => '',
-                    'Pembuat' => '',
-                ];
             }
 
 
@@ -170,8 +171,8 @@ class ExportReportRecapTax implements FromCollection, WithTitle, WithHeadings, S
                 '% Diskon' => '',
                 '% Diskon 2' => '',
                 'Diskon 3' => '',
-                'DPP Diskon' => $dpp_discount_total,
-                'Total Harga Barang (DPP)' => $price_dpp_total,
+                'DPP Diskon' => '',
+                'Total Harga Barang (DPP)' => '',
                 'Uang Muka (DP)' => $row->downpayment,
                 'Total' =>  $row->total,
                 'DPP FP' => $dpp_total,

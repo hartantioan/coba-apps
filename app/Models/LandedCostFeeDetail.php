42e145ae0abb64b318f932dd791685fd17353877
Yustinus Windy <windykuro@gmail.com>
2024-11-18 14:41:41 +0700

    Update LandedCostFeeDetail.php

--- app/Models/LandedCostFeeDetail.php
+++ app/Models/LandedCostFeeDetail.php
@@ -39,14 +39,14 @@ class LandedCostFeeDetail extends Model
     public function purchaseInvoiceDetail()
     {
         return $this->hasMany('App\Models\PurchaseInvoiceDetail','lookable_id','id')->where('lookable_type',$this->table)->whereHas('purchaseInvoice',function($query){
-            $query->whereIn('status',['1','2','3','8']);
+            $query->whereIn('status',['1','2','3','7','8']);
         });
     }
 
     public function purchaseInvoiceRealDetail()
     {
         return $this->hasMany('App\Models\PurchaseInvoiceDetail','lookable_id','id')->where('lookable_type',$this->table)->whereHas('purchaseInvoice',function($query){
-            $query->whereIn('status',['2','3','8']);
+            $query->whereIn('status',['2','3','7','8']);
         });
     }
 

4a3ab16998d3c6851cc52c9dd98793322e687ffb
Yustinus Windy <windykuro@gmail.com>
2024-11-22 14:53:38 +0700

    Update MarketingOrderDeliveryController.php

--- app/Http/Controllers/Sales/MarketingOrderDeliveryController.php
+++ app/Http/Controllers/Sales/MarketingOrderDeliveryController.php
@@ -491,9 +491,8 @@ class MarketingOrderDeliveryController extends Controller
                         } */
                         $itemShading = ItemShading::find($row);
                         if($itemShading){
-                            $stock = round($itemShading->stockAvailable(),3);
-                            $needed = round($arrShadingQty[$key] * $arrConversion[$key],3);
-                            if($stock < $needed){
+                            $stock = round($itemShading->stockAvailable()/$arrConversion[$key],3);
+                            if($stock < round($arrShadingQty[$key],3)){
                                 $arrItemError[] = $itemShading->item->name.' Shading '.$itemShading->code.' Kebutuhan '.CustomHelper::formatConditionalQty(round($arrShadingQty[$key],3)).' Stok : '.CustomHelper::formatConditionalQty($stock);
                                 $passedQtyShading = false;
                             }

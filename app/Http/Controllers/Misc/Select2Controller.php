162a05eff29aedb20d10104059eb88d9ba3a3d19
Yustinus Windy <windykuro@gmail.com>
2024-11-26 15:38:51 +0700

    Update Select2Controller.php

--- app/Http/Controllers/Misc/Select2Controller.php
+++ app/Http/Controllers/Misc/Select2Controller.php
@@ -602,11 +602,7 @@ class Select2Controller extends Controller {
                 'code'              => $d->code,
                 'name'              => $d->name,
                 'uom'               => $d->uomUnit->code,
-                'price_list'        => $d->currentCogs($this->dataplaces),
                 'stock_list'        => $d->currentStockMoreThanZero($this->dataplaces,$this->datawarehouses),
-                'list_warehouse'    => $d->warehouseList(),
-                'is_sales_item'     => $d->is_sales_item ? $d->is_sales_item : '',
-                'list_shading'      => $d->arrShading(),
                 'is_activa'         => $d->itemGroup->is_activa ? $d->itemGroup->is_activa : '',
             ];
         }

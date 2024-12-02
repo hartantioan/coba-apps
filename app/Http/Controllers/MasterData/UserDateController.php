67a2747097bc5a9f973391117d29bf560a1c7593
Yustinus Windy <windykuro@gmail.com>
2024-12-02 16:49:27 +0700

    Update UserDateController.php

--- app/Http/Controllers/MasterData/UserDateController.php
+++ app/Http/Controllers/MasterData/UserDateController.php
@@ -125,7 +125,7 @@ class UserDateController extends Controller
 
     public function create(Request $request){
         $validation = Validator::make($request->all(), [
-            'code' 				=> $request->temp ? ['required', Rule::unique('user_dates', 'code')->whereNull('deleted_at')->ignore($request->temp)] : ['required', Rule::unique('user_dates', 'code')->whereNull('deleted_at')],
+            'code' 				=> $request->temp ? ['required', Rule::unique('user_dates', 'code')->ignore($request->temp)] : 'required|unique:user_dates,code',
             'name'              => 'required',
             'count_backdate'    => 'required',
             'count_futuredate'  => 'required',

3b118a2b0602672642b4934b1fccc1078414505d
Yustinus Windy <windykuro@gmail.com>
2024-12-04 10:38:32 +0700

    rounding

--- app/Models/ItemStock.php
+++ app/Models/ItemStock.php
@@ -46,9 +46,9 @@ class ItemStock extends Model
         $cogs = ItemCogs::where('item_id',$this->item_id)->where('place_id',$this->place_id)->where('warehouse_id',$this->warehouse_id)->where('item_shading_id',$this->item_shading_id)->where('production_batch_id',$this->production_batch_id)->where('area_id',$this->area_id)->where('date','<=',$date)->orderBy('date')->orderBy('id')->get();
         foreach($cogs as $row){
             if($row->type == 'IN'){
-                $qty += $row->qty_in;
+                $qty += round($row->qty_in,3);
             }elseif($row->type == 'OUT'){
-                $qty -= $row->qty_out;
+                $qty -= round($row->qty_out,3);
             }
         }
         return $qty;

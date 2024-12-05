45492552f24261d950403b33f2c754987237ea36
Yustinus Windy <windykuro@gmail.com>
2024-12-05 19:30:17 +0700

    Update DownPaymentController.php

--- app/Http/Controllers/Purchase/DownPaymentController.php
+++ app/Http/Controllers/Purchase/DownPaymentController.php
@@ -171,7 +171,7 @@ class DownPaymentController extends Controller
             $currency_rate = $row->latest_currency > 0 ? $row->latest_currency : $row->currency_rate;
             $total_received_after_adjust = round($row->grandtotal * $currency_rate, 2);
             $total_invoice_after_adjust = round(($row->total_used + $row->total_memo) * $currency_rate,2);
-            $balance_after_adjust = round($total_received_after_adjust - $total_invoice_after_adjust - $row->total_journal_debit + $row->total_journal_credit,2);
+            $balance_after_adjust = round($total_received_after_adjust - $total_invoice_after_adjust + $row->total_journal_debit - $row->total_journal_credit,2);
             $balance = round($row->grandtotal - $row->total_used - $row->total_memo,2);
             $currency_rate = $row->latest_currency;
             /* $balance_rp = round($balance * $currency_rate,2) + $row->adjust_nominal - $row->total_journal; */

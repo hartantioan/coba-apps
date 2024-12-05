7cd5d3c01967c0249f816cdf90be30f496536f95
Yustinus Windy <windykuro@gmail.com>
2024-12-05 11:23:08 +0700

    Update PurchaseInvoiceDetail.php

--- app/Models/PurchaseInvoiceDetail.php
+++ app/Models/PurchaseInvoiceDetail.php
@@ -341,6 +341,12 @@ class PurchaseInvoiceDetail extends Model
         });
     }
 
+    public function journalDetail(){
+        return $this->hasMany('App\Models\JournalDetail','detailable_id','id')->where('detailable_type',$this->table)->whereHas('journal',function($query){
+            $query->whereIn('status',['2','3']);
+        });
+    }
+
     public function balanceMemo(){
         $total = $this->total;
 

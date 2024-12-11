f143ba1338ee926d5150846bf3f711281cba579a
hartantioan <129711861+hartantioan@users.noreply.github.com>
2024-12-11 09:21:38 +0700

    Create 2024_12_09_094320_add_column_truck_queues.php

---
+++ database/migrations/2024_12_09_094320_add_column_truck_queues.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('truck_queues', function (Blueprint $table) {
+            $table->dateTime('time_load_fg')->nullable();
+            $table->dateTime('time_done_load_fg')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

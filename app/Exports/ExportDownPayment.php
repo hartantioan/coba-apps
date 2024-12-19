ab08ad6c70072978d481dea297a75e92f136e519
Yustinus Windy <windykuro@gmail.com>
2024-12-19 20:26:28 +0700

    Update ExportDownPayment.php

--- app/Exports/ExportDownPayment.php
+++ app/Exports/ExportDownPayment.php
@@ -27,124 +27,158 @@ class ExportDownPayment implements FromView,ShouldAutoSize
         $totalbalance=0;
         $array_filter = [];
         $query_data = DB::select("
+                    SELECT 
+                *,
+                pdp.type as typepdp,
+                IFNULL((SELECT 
+                    SUM(nominal) 
+                    FROM purchase_invoice_dps pid 
+                    JOIN purchase_invoices pi
+                        ON pid.purchase_invoice_id = pi.id
+                    WHERE 
+                        pid.purchase_down_payment_id = pdp.id
+                        AND pi.post_date <= :date1
+                        AND pi.status IN ('2','3','7')
+                        AND pid.deleted_at IS NULL
+                ),0) AS total_used,
+                IFNULL((
                     SELECT
-                    *,
-                    pdp.type as typepdp,
-                    IFNULL((SELECT
-                        SUM(nominal)
-                        FROM purchase_invoice_dps pid
-                        JOIN purchase_invoices pi
-                            ON pid.purchase_invoice_id = pi.id
-                        WHERE
-                            pid.purchase_down_payment_id = pdp.id
-                            AND pi.post_date <= :date1
-                            AND pi.status IN ('2','3','7')
-                            AND pid.deleted_at IS NULL
-                    ),0) AS total_used,
-                    IFNULL((
-                        SELECT
-                            SUM(pmd.grandtotal)
-                            FROM purchase_memo_details pmd
-                            JOIN purchase_memos pm
-                                ON pm.id = pmd.purchase_memo_id
-                            WHERE pmd.lookable_type = 'purchase_down_payments'
-                            AND pmd.lookable_id = pdp.id
-                            AND pm.post_date <= :date2
-                            AND pm.status IN ('2','3','7')
-                    ),0) AS total_memo,
-                    IFNULL((SELECT
-                        SUM(ROUND(ard.nominal,2))
-                        FROM adjust_rate_details ard
-                        JOIN adjust_rates ar
-                            ON ar.id = ard.adjust_rate_id
-                        WHERE
-                            ar.post_date <= :date3
-                            AND ar.status IN ('2','3')
-                            AND ard.lookable_type = 'purchase_down_payments'
-                            AND ard.lookable_id = pdp.id
-                            AND (
-                                CASE
-                                    WHEN ar.post_date >= '2024-06-01' THEN ard.type = '1'
-                                    WHEN ar.post_date < '2024-06-01' THEN ard.type IS NOT NULL
-                                END
-                            )
-                    ),0) AS adjust_nominal,
-                    IFNULL((SELECT
-                        ar.currency_rate
-                        FROM adjust_rate_details ard
-                        JOIN adjust_rates ar
-                            ON ar.id = ard.adjust_rate_id
-                        WHERE
-                            ar.post_date <= :date4
-                            AND ar.status IN ('2','3')
-                            AND ard.lookable_type = 'purchase_down_payments'
-                            AND ard.lookable_id = pdp.id
-                        ORDER BY ar.id DESC
-                        LIMIT 1
-                    ),0) AS latest_currency,
-                    IFNULL((SELECT
-                        ar.reverse_date
-                        FROM adjust_rate_details ard
-                        JOIN adjust_rates ar
-                            ON ar.id = ard.adjust_rate_id
-                        WHERE 
-                            ar.post_date <= :date5
-                            AND ar.status IN ('2','3')
-                            AND ard.lookable_type = 'purchase_down_payments'
-                            AND ard.lookable_id = pdp.id
-                        ORDER BY ar.id DESC
-                        LIMIT 1
-                    ),'') AS latest_reverse_date,
-                    IFNULL((
-                        SELECT
-                            SUM(jd.nominal)
-                            FROM journal_details jd
-                            JOIN journals j
-                                ON j.id = jd.journal_id
-                            JOIN coas c
-                                ON jd.coa_id = c.id
-                            WHERE c.code = '100.01.07.01.01'
-                            AND jd.note = CONCAT('REVERSE*',pdp.code)
-                            AND j.post_date <= :date6
-                            AND j.status IN ('2','3')
-                            AND jd.deleted_at IS NULL
-                            AND jd.type = '1'
-                    ),0) AS total_journal_debit,
-                    IFNULL((
-                        SELECT
-                            SUM(ROUND(jd.nominal,2))
-                            FROM journal_details jd
-                            JOIN journals j
-                                ON j.id = jd.journal_id
-                            JOIN coas c
-                                ON jd.coa_id = c.id
-                            WHERE c.code = '100.01.07.01.01'
-                            AND jd.note = CONCAT('REVERSE*',pdp.code)
-                            AND j.post_date <= :date7
-                            AND j.status IN ('2','3')
-                            AND jd.deleted_at IS NULL
-                            AND jd.type = '2'
-                    ),0) AS total_journal_credit,
-                    u.name AS account_name,
-                    u.employee_no AS account_code,
-                    uvoid.name AS void_name,
-                    udelete.name AS delete_name
-                    FROM purchase_down_payments pdp
-                    LEFT JOIN users u
-                        ON u.id = pdp.account_id
-                    LEFT JOIN users uvoid
-                        ON uvoid.id = pdp.void_id
-                    LEFT JOIN users udelete
-                        ON udelete.id = pdp.void_id
-                    WHERE
-                        pdp.post_date <= :date8
-                        AND pdp.grandtotal > 0
-                        AND pdp.status IN ('2','3','7','8')
-                        AND IFNULL((SELECT
+                        SUM(pmd.grandtotal)
+                        FROM purchase_memo_details pmd
+                        JOIN purchase_memos pm
+                            ON pm.id = pmd.purchase_memo_id
+                        WHERE pmd.lookable_type = 'purchase_down_payments'
+                        AND pmd.lookable_id = pdp.id
+                        AND pm.post_date <= :date2
+                        AND pm.status IN ('2','3','7')
+                ),0) AS total_memo,
+                IFNULL((SELECT
+                    SUM(ROUND(ard.nominal,2))
+                    FROM adjust_rate_details ard
+                    JOIN adjust_rates ar
+                        ON ar.id = ard.adjust_rate_id
+                    WHERE 
+                        ar.post_date <= :date3
+                        AND ar.status IN ('2','3')
+                        AND ard.lookable_type = 'purchase_down_payments'
+                        AND ard.lookable_id = pdp.id
+                        AND (
+                            CASE 
+                                WHEN ar.post_date >= '2024-06-01' THEN ard.type = '1'
+                                WHEN ar.post_date < '2024-06-01' THEN ard.type IS NOT NULL
+                            END
+                        )
+                ),0) AS adjust_nominal,
+                IFNULL((SELECT
+                    SUM(ROUND(ard.nominal,2))
+                    FROM adjust_rate_details ard
+                    JOIN adjust_rates ar
+                        ON ar.id = ard.adjust_rate_id
+                    WHERE 
+                        ar.post_date <= :date4
+                        AND ar.post_date < '2024-11-30'
+                        AND ar.status IN ('2','3')
+                        AND ard.lookable_type = 'purchase_down_payments'
+                        AND ard.lookable_id = pdp.id
+                        AND ard.type = '1'
+                ),0) AS adjust_nominal_latest,
+                IFNULL((SELECT
+                    SUM(ROUND(ard.nominal,2))
+                    FROM adjust_rate_details ard
+                    JOIN adjust_rates ar
+                        ON ar.id = ard.adjust_rate_id
+                    WHERE 
+                        ar.post_date <= :date5
+                        AND ar.post_date >= '2024-11-30'
+                        AND ar.status IN ('2','3')
+                        AND ard.lookable_type = 'purchase_down_payments'
+                        AND ard.lookable_id = pdp.id
+                        AND ard.type = '1'
+                ),0) AS adjust_nominal_after_update,
+                IFNULL((SELECT
+                    ar.currency_rate
+                    FROM adjust_rate_details ard
+                    JOIN adjust_rates ar
+                        ON ar.id = ard.adjust_rate_id
+                    WHERE 
+                        ar.post_date <= :date6
+                        AND ar.status IN ('2','3')
+                        AND ard.lookable_type = 'purchase_down_payments'
+                        AND ard.lookable_id = pdp.id
+                    ORDER BY ar.id DESC
+                    LIMIT 1
+                ),0) AS latest_currency,
+                IFNULL((SELECT
+                    ar.currency_rate
+                    FROM adjust_rate_details ard
+                    JOIN adjust_rates ar
+                        ON ar.id = ard.adjust_rate_id
+                    WHERE 
+                        ar.post_date <= :date7
+                        AND ar.status IN ('2','3')
+                        AND ard.lookable_type = 'purchase_down_payments'
+                        AND ard.lookable_id = pdp.id
+                        AND ar.post_date < '2024-11-30'
+                    ORDER BY ar.id DESC
+                    LIMIT 1
+                ),0) AS latest_currency_before,
+                IFNULL((SELECT
+                    ar.reverse_date
+                    FROM adjust_rate_details ard
+                    JOIN adjust_rates ar
+                        ON ar.id = ard.adjust_rate_id
+                    WHERE 
+                        ar.post_date <= :date8
+                        AND ar.status IN ('2','3')
+                        AND ard.lookable_type = 'purchase_down_payments'
+                        AND ard.lookable_id = pdp.id
+                    ORDER BY ar.id DESC
+                    LIMIT 1
+                ),'') AS latest_reverse_date,
+                IFNULL((
+                    SELECT
+                        SUM(ROUND(jd.nominal,2))
+                        FROM journal_details jd
+                        JOIN journals j
+                            ON j.id = jd.journal_id
+                        JOIN coas c
+                            ON jd.coa_id = c.id
+                        WHERE c.code = '100.01.07.01.01'
+                        AND jd.note = CONCAT('REVERSE*',pdp.code)
+                        AND j.post_date <= :date9
+                        AND j.status IN ('2','3')
+                        AND jd.deleted_at IS NULL
+                        AND jd.type = '1'
+                ),0) AS total_journal_debit,
+                IFNULL((
+                    SELECT
+                        SUM(ROUND(jd.nominal,2))
+                        FROM journal_details jd
+                        JOIN journals j
+                            ON j.id = jd.journal_id
+                        JOIN coas c
+                            ON jd.coa_id = c.id
+                        WHERE c.code = '100.01.07.01.01'
+                        AND jd.note = CONCAT('REVERSE*',pdp.code)
+                        AND j.post_date <= :date10
+                        AND j.status IN ('2','3')
+                        AND jd.deleted_at IS NULL
+                        AND jd.type = '2'
+                ),0) AS total_journal_credit,
+                u.name AS account_name,
+                u.employee_no AS account_code
+                FROM purchase_down_payments pdp
+                LEFT JOIN users u
+                    ON u.id = pdp.account_id
+                WHERE 
+                    pdp.post_date <= :date11
+                    AND pdp.grandtotal > 0
+                    AND pdp.status IN ('2','3','7','8')
+                    AND IFNULL((SELECT
                         '1'
                         FROM cancel_documents cd
-                        WHERE
-                            cd.post_date <= :date9
+                        WHERE 
+                            cd.post_date <= :date12
                             AND cd.lookable_type = 'purchase_down_payments'
                             AND cd.lookable_id = pdp.id
                             AND cd.deleted_at IS NULL
@@ -159,19 +193,33 @@ class ExportDownPayment implements FromView,ShouldAutoSize
                     'date7' => $this->date,
                     'date8' => $this->date,
                     'date9' => $this->date,
+                    'date10' => $this->date,
+                    'date11' => $this->date,
+                    'date12' => $this->date,
                 ));
 
             foreach($query_data as $row_invoice){
-                $currency_rate = $row_invoice->latest_currency > 0 ? $row_invoice->latest_currency : $row_invoice->currency_rate;
-                $total_adjust_new_rule = 0;
-                if($row_invoice->latest_reverse_date >= '2024-11-01' && $row_invoice->post_date >= '2024-10-01'){
-                    $total_adjust_new_rule = round(($row_invoice->total_used / $row_invoice->grandtotal) * $row_invoice->adjust_nominal,2);
+                if($this->date >= '2024-11-30'){
+                    $currency_rate = $row_invoice->latest_currency_before > 0 ? $row_invoice->latest_currency_before : $row_invoice->currency_rate;
+                    $total_adjust_new_rule = 0;
+                    if($row_invoice->latest_reverse_date >= '2024-11-01' && $row_invoice->post_date >= '2024-10-01'){
+                        $total_adjust_new_rule = round(($row_invoice->total_used / $row_invoice->grandtotal) * $row_invoice->adjust_nominal_latest,2);
+                    }
+                    $total_received_after_adjust = round($row_invoice->grandtotal * $currency_rate, 2) + $row_invoice->adjust_nominal_after_update;
+                    $total_invoice_after_adjust = round(($row_invoice->total_used + $row_invoice->total_memo) * $currency_rate,2);
+                    $balance_after_adjust = round($total_received_after_adjust - $total_invoice_after_adjust + $row_invoice->total_journal_debit - $row_invoice->total_journal_credit,2) + $total_adjust_new_rule;
+                    $balance = round($row_invoice->grandtotal - $row_invoice->total_used - $row_invoice->total_memo,2);
+                }else{
+                    $currency_rate = $row_invoice->latest_currency > 0 ? $row_invoice->latest_currency : $row_invoice->currency_rate;
+                    $total_adjust_new_rule = 0;
+                    if($row_invoice->latest_reverse_date >= '2024-11-01' && $row_invoice->post_date >= '2024-10-01'){
+                        $total_adjust_new_rule = round(($row_invoice->total_used / $row_invoice->grandtotal) * $row_invoice->adjust_nominal,2);
+                    }
+                    $total_received_after_adjust = round($row_invoice->grandtotal * $currency_rate, 2);
+                    $total_invoice_after_adjust = round(($row_invoice->total_used + $row_invoice->total_memo) * $currency_rate,2);
+                    $balance_after_adjust = round($total_received_after_adjust - $total_invoice_after_adjust + $row_invoice->total_journal_debit - $row_invoice->total_journal_credit,2) + $total_adjust_new_rule;
+                    $balance = round($row_invoice->grandtotal - $row_invoice->total_used - $row_invoice->total_memo,2);
                 }
-                $total_received_after_adjust = round($row_invoice->grandtotal * $currency_rate,2);
-                $total_invoice_after_adjust = round(($row_invoice->total_used + $row_invoice->total_memo) * $currency_rate,2);
-                $balance_after_adjust = round($total_received_after_adjust - $total_invoice_after_adjust + $row_invoice->total_journal_debit - $row_invoice->total_journal_credit,2) + $total_adjust_new_rule;
-                $balance = round($row_invoice->grandtotal - $row_invoice->total_used - $row_invoice->total_memo,2);
-                $currency_rate = $row_invoice->latest_currency;
                 if($balance > 0){
                     $pdp = PurchaseDownPayment::where('code',$row_invoice->code)->first();
                     $array_filter[] = [
@@ -182,12 +230,6 @@ class ExportDownPayment implements FromView,ShouldAutoSize
                         'post_date'     => date('d/m/Y',strtotime($row_invoice->post_date)),
                         'due_date'      => date('d/m/Y',strtotime($row_invoice->due_date)),
                         'note'          => $row_invoice->note,
-                        'subtotal'      => number_format($row_invoice->subtotal * $currency_rate,2,',','.'),
-                        'discount'      => number_format($row_invoice->discount * $currency_rate,2,',','.'),
-                        'total_fc'         => number_format($row_invoice->grandtotal * $currency_rate,2,',','.'),
-                        'total'      => number_format($row_invoice->grandtotal,2,',','.'),
-                        'used'          => number_format($row_invoice->total_used * $currency_rate,2,',','.'),
-                        'memo'          => number_format($row_invoice->total_memo * $currency_rate,2,',','.'),
                         'balance'       => number_format($balance_after_adjust,2,',','.'),
                         'balance_fc'    => number_format($balance,2,',','.'),
                         'status'        => $this->getStatus($row_invoice->status),

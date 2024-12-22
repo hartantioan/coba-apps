a08820b0169217e51a75f7e85d006fc62281cf65
Yustinus Windy <windykuro@gmail.com>
2024-12-22 13:52:26 +0700

    Update ProductionIssueController.php

--- app/Http/Controllers/Production/ProductionIssueController.php
+++ app/Http/Controllers/Production/ProductionIssueController.php
@@ -299,8 +299,8 @@ class ProductionIssueController extends Controller
     }
 
     public function create(Request $request){
-        /* DB::beginTransaction();
-        try { */
+        DB::beginTransaction();
+        try {
             $validation = Validator::make($request->all(), [
                 'code'                      => 'required',
                 'code_place_id'             => 'required',
@@ -594,10 +594,10 @@ class ProductionIssueController extends Controller
                 }
             }
 
-            /* DB::commit();
+            DB::commit();
         }catch(\Exception $e){
             DB::rollback();
-        } */
+        }
 
 		return response()->json($response);
     }

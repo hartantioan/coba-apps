0daa15d36cfc8e6387b4e82160dee7e499782637
hartantioan <129711861+hartantioan@users.noreply.github.com>
2024-12-22 13:14:05 +0700

    faster load for mod

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -448,23 +449,53 @@ class User extends Authenticatable
     }
 
     public function grandtotalUnsentModCredit(){
-        $totalMod = 0;
-        foreach($this->marketingOrder as $row){
-            foreach($row->marketingOrderDetail as $rowdetail){
-                foreach($rowdetail->marketingOrderDeliveryDetail()->whereHas('marketingOrderDelivery',function($query){
-                    $query->where('status','2');
-                })->get() as $rowmod){
-                    $totalMod += round(($rowmod->getGrandtotal() * ((100 - $row->percent_dp)/100)),2);
-                }
-            }
-        }
-
-        return $totalMod;
+        $query = "
+                    SELECT SUM(
+            ROUND(
+                (
+                    CASE
+                        WHEN modtdelivery.tax_id > 0 AND modtdelivery.is_include_tax = '1'
+                        THEN
+                            (modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion) / (1 + (modtdelivery.percent_tax / 100))
+                        ELSE
+                            modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion
+                    END
+                )
+                +
+                CASE
+                    WHEN modtdelivery.tax_id > 0 THEN
+                        -- Use the same logic for total_case_1 here
+                        (
+                            CASE
+                                WHEN modtdelivery.tax_id > 0 AND modtdelivery.is_include_tax = '1'
+                                THEN
+                                    (modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion) / (1 + (modtdelivery.percent_tax / 100))
+                                ELSE
+                                    modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion
+                            END
+                        ) * (modtdelivery.percent_tax / 100)
+                    ELSE
+                        0
+                END
+                , 2) * ((100 - mo.percent_dp) / 100)
+        ) AS totalMod
+        FROM marketing_orders mo
+        JOIN marketing_order_details modetail ON modetail.marketing_order_id = mo.id
+        JOIN marketing_order_delivery_details modeliverydetail ON modeliverydetail.marketing_order_detail_id = modetail.id
+        JOIN marketing_order_details modtdelivery ON modeliverydetail.marketing_order_detail_id = modtdelivery.id
+        JOIN marketing_order_deliveries moddelivery ON moddelivery.id = modeliverydetail.marketing_order_delivery_id
+        WHERE moddelivery.status = 2
+        AND mo.account_id = :account_id;
+        ";
+        $totalMod = DB::select($query, ['account_id' => $this->id]);
+        $totalModValue = $totalMod[0]->totalMod ?? 0;
+        return $totalModValue;
     }
 
     public function grandtotalUnsentModDp(){
         $totalMod = 0;
         foreach($this->marketingOrder()->where('percent_dp','>',0)->get() as $row){
+
             foreach($row->marketingOrderDetail as $rowdetail){
                 foreach($rowdetail->marketingOrderDeliveryDetail()->whereHas('marketingOrderDeliveryProcessDetail',function($query){
                     $query->whereHas('marketingOrderDeliveryProcess',function($query){
@@ -475,7 +506,6 @@ class User extends Authenticatable
                 }
             }
         }
-
         return $totalMod;
     }
 
@@ -497,20 +527,60 @@ class User extends Authenticatable
     }
 
     public function grandtotalUninvoiceDoCredit(){
-        $totalDo = 0;
-        foreach($this->marketingOrder as $row){
-            foreach($row->marketingOrderDetail as $rowdetail){
-                foreach($rowdetail->marketingOrderDeliveryDetail()->whereHas('marketingOrderDeliveryProcessDetail',function($query){
-                    $query->whereHas('marketingOrderDeliveryProcess',function($query){
-                        $query->where('status','2');
-                    });
-                })->get() as $rowmod){
-                    $totalDo += round(($rowmod->getGrandtotal() * ((100 - $row->percent_dp)/100)),2);
-                }
-            }
-        }
-
-        return $totalDo;
+        $query = "
+            SELECT
+                SUM(modified_grandtotal) AS totalMod
+            FROM (
+                SELECT
+                    ROUND(
+                        (
+                            CASE
+                                WHEN modtdelivery.tax_id > 0 AND modtdelivery.is_include_tax = '1'
+                                THEN
+                                    (modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion) / (1 + (modtdelivery.percent_tax / 100))
+                                ELSE
+                                    modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion
+                            END
+                        )
+                        +
+                        CASE
+                            WHEN modtdelivery.tax_id > 0 THEN
+                                (
+                                    CASE
+                                        WHEN modtdelivery.tax_id > 0 AND modtdelivery.is_include_tax = '1'
+                                        THEN
+                                            (modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion) / (1 + (modtdelivery.percent_tax / 100))
+                                        ELSE
+                                            modeliverydetail.qty * modtdelivery.price_after_discount * modtdelivery.qty_conversion
+                                    END
+                                ) * (modtdelivery.percent_tax / 100)
+                            ELSE
+                                0
+                        END
+                        , 2) * ((100 - mo.percent_dp) / 100) AS modified_grandtotal
+                FROM marketing_orders mo
+                JOIN marketing_order_details modetail ON modetail.marketing_order_id = mo.id
+                JOIN marketing_order_delivery_details modeliverydetail ON modeliverydetail.marketing_order_detail_id = modetail.id
+                JOIN marketing_order_details modtdelivery ON modeliverydetail.marketing_order_detail_id = modtdelivery.id
+                JOIN marketing_order_deliveries moddelivery ON moddelivery.id = modeliverydetail.marketing_order_delivery_id
+                JOIN marketing_order_delivery_process_details modeliveryprocessdetail ON modeliveryprocessdetail.marketing_order_delivery_detail_id = modeliverydetail.id
+                JOIN marketing_order_delivery_processes modeliveryprocess ON modeliveryprocess.id = modeliveryprocessdetail.marketing_order_delivery_process_id
+                WHERE modeliveryprocess.status = 2
+                AND mo.account_id = :account_id
+                GROUP BY
+                    modetail.id,
+                    modeliverydetail.qty,
+                    modtdelivery.price_after_discount,
+                    modtdelivery.qty_conversion,
+                    modtdelivery.tax_id,
+                    modtdelivery.is_include_tax,
+                    modtdelivery.percent_tax,
+                    mo.percent_dp
+            ) AS grouped_orders;
+        ";
+        $totalMod = DB::select($query, ['account_id' => $this->id]);
+        $totalModValue = $totalMod[0]->totalMod ?? 0;
+        return $totalModValue;
     }
 
     public function grandtotalUninvoiceDoDp(){

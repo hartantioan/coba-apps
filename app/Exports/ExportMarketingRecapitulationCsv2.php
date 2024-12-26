4b25dcb9b1fff5e3d2752be2431266a2ca6ac3bc
Yustinus Windy <windykuro@gmail.com>
2024-12-26 11:39:28 +0700

    Update ExportMarketingRecapitulationCsv2.php

--- app/Exports/ExportMarketingRecapitulationCsv2.php
+++ app/Exports/ExportMarketingRecapitulationCsv2.php
@@ -17,7 +17,7 @@ use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
 use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
-class ExportMarketingRecapitulationCsv2 extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder implements FromCollection
+class ExportMarketingRecapitulationCsv2 extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder implements FromCollection, WithCustomValueBinder
 {
     /**
      * @return \Illuminate\Support\Collection

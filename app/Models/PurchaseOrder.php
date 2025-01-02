a9bc2d57bb6e87f59f6b02526af45b252b06cbf8
Yustinus Windy <windykuro@gmail.com>
2025-01-02 11:02:49 +0700

    Update PurchaseOrder.php

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -276,6 +276,7 @@ class PurchaseOrder extends Model
         $query = PurchaseOrder::selectRaw('RIGHT(code, 8) as code')
             ->whereRaw("code LIKE '$cek%'")
             ->withTrashed()
+            ->orderByDesc('code')
             ->orderByDesc('id')
             ->limit(1)
             ->get();

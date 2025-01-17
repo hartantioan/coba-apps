39e5e260ebc117ca849fd1405085fd8418525643
Yustinus Windy <windykuro@gmail.com>
2025-01-17 13:58:19 +0700

    Update GoodScaleController.php

--- app/Http/Controllers/Inventory/GoodScaleController.php
+++ app/Http/Controllers/Inventory/GoodScaleController.php
@@ -1616,7 +1616,7 @@ class GoodScaleController extends Controller
                 'company'   => $query->company()->exists() ? $query->company->name : '-',
                 'code'      => $query->journal->code,
                 'note'      => $query->note,
-                'post_date' => date('d/m/Y',strtotime($query->post_date)),
+                'post_date' => date('d/m/Y',strtotime($query->journal->post_date)),
             ];
             $string='';
             foreach($query->journal->journalDetail()->where(function($query){

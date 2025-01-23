c071ce9b708673c09fc7ccb8121b13d6a04acf88
Yustinus Windy <windykuro@gmail.com>
2025-01-23 15:12:55 +0700

    Update order.blade.php

--- resources/views/admin/sales/order.blade.php
+++ resources/views/admin/sales/order.blade.php
@@ -1589,6 +1589,8 @@
                             );
                         }
                     }
+
+                    countRow(nil);
                 },
                 error: function() {
                     loadingClose('#modal1');

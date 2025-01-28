0b32aa54e39a48c1b0d72b602c42d1bdccb5588a
Yustinus Windy <windykuro@gmail.com>
2025-01-28 10:01:32 +0700

    Update landed_cost.blade.php

--- resources/views/admin/purchase/landed_cost.blade.php
+++ resources/views/admin/purchase/landed_cost.blade.php
@@ -2260,7 +2260,7 @@
                     render: function (data, type, full, meta) {
                         return "<div class='text-wrap width-300'>" + data + "</div>";
                     },
-                    targets: 16,10
+                    targets: 16
                 }
             ],
         });

f9658faf264da2fec7307c54f9be97c6bd8bbcb3
Yustinus Windy <windykuro@gmail.com>
2025-01-29 14:03:48 +0700

    Update good_issue_request.blade.php

--- resources/views/admin/inventory/good_issue_request.blade.php
+++ resources/views/admin/inventory/good_issue_request.blade.php
@@ -643,7 +643,35 @@
         });
 
         $('#arr_place0,#arr_department0').formSelect();
-        select2ServerSide('#arr_item0', '{{ url("admin/select2/inventory_item") }}');
+        $('arr_item0').select2({
+            placeholder: '-- Pilih ya --',
+            minimumInputLength: 4,
+            allowClear: true,
+            cache: true,
+            width: 'resolve',
+            dropdownParent: $('body').parent(),
+            ajax: {
+                url: '{{ url("admin/select2/inventory_item") }}',
+                type: 'GET',
+                dataType: 'JSON',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        search: params.term,
+                    };
+                },
+                processResults: function(data, params) {
+                    params.page = params.page || 1;
+                    return {
+                        results: data.items,
+                        pagination: {
+                            more: data.pagination.more
+                        }
+                    };
+                },
+                cache: true,
+            }
+        });
         select2ServerSide('#arr_project0', '{{ url("admin/select2/project") }}');
 
         $("#table-detail th").resizable({

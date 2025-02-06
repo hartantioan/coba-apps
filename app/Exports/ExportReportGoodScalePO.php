aa7a192610e51d60f5d284b84884cd21b7b7afb8
Yustinus Windy <windykuro@gmail.com>
2025-02-06 20:48:41 +0700

    Update ExportReportGoodScalePO.php

--- app/Exports/ExportReportGoodScalePO.php
+++ app/Exports/ExportReportGoodScalePO.php
@@ -101,7 +101,7 @@ class ExportReportGoodScalePO implements FromCollection, WithTitle, WithHeadings
                 $po_code =$row->goodScale->purchaseOrder->code;
                 $list = $row->goodScale->purchaseOrder->getInvoice();
                 $subtotal = $row->lookable->marketingOrderDeliveryProcess()->exists() && $row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail()->exists() ?$row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail->subtotal * $row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail->purchaseOrder->currency_rate : 0;
-                $discount = $row->lookable->marketingOrderDeliveryProcess()->exists() ?$row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail->discountHeader() * $row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail->purchaseOrder->currency_rate : 0;
+                $discount = $row->lookable->marketingOrderDeliveryProcess()->exists() && $row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail()->exists() ?$row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail->discountHeader() * $row->lookable->marketingOrderDeliveryProcess->purchaseOrderDetail->purchaseOrder->currency_rate : 0;
                 $po_total = $subtotal-$discount;
             }
 

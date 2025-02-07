ad60e06d12eca709d147da48eb5e49f4e5fa2242
hartantioan <129711861+hartantioan@users.noreply.github.com>
2025-02-07 14:04:09 +0700

    Update ExportReportProcurement.php

--- app/Exports/ExportReportProcurement.php
+++ app/Exports/ExportReportProcurement.php
@@ -166,13 +166,13 @@ class ExportReportProcurement implements FromCollection, WithTitle, WithHeadings
                     $take_item_rule_percent = RuleBpScale::where('item_id',$this->item_id)
                     ->whereDate('start_effective_date','<=',$row->goodScale->post_date)
                     ->whereDate('effective_date','>=',$row->goodScale->post_date)
-                    ->where('account_id',$row->purchaseOrderDetail->purchaseOrder->account_id)->first();
+                    ->where('account_id',$row->goodScale->account_id)->first();
                 }
                 $percentage_level = 0;
                 $percentage_netto_limit = 0;
                 $finance_kadar_air = 0;
                 $finance_kg = 0;
-                if($take_item_rule_percent){
+                if($take_item_rule_percent && !$row->percent_modifier){
                     $percentage_level = round($take_item_rule_percent->percentage_level,2);
                     $percentage_netto_limit = round($take_item_rule_percent->percentage_netto_limit,2);
                 }

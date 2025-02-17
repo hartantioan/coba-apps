2a697be8c1b5999e9241c5792534619f0eb983c9
hartantioan <129711861+hartantioan@users.noreply.github.com>
2025-02-17 15:39:42 +0700

    procurement multi item

--- resources/views/admin/purchase/report_procurement.blade.php
+++ resources/views/admin/purchase/report_procurement.blade.php
@@ -1,4 +1,15 @@
-<!-- BEGIN: Page Main-->
+<style>
+    .select-wrapper, .select2-container {
+        height:3.7rem !important;
+    }
+    .select2-selection--multiple{
+        overflow-y: scroll !important;
+        height: auto !important;
+    }
+    .select2{
+        height: fit-content !important;
+    }
+</style>
 <div id="main">
     <div class="row">
         <div class="pt-3 pb-1" id="breadcrumbs-wrapper">
@@ -89,62 +100,55 @@
                                 </li>
                             </ul>
                         </div>
-                        {{-- <div class="card">
-                            <div class="card-content">
-                                <h4 class="card-title">
-                                    Hasil
-                                </h4>
-                                <div class="row">
-                                    <div class="col s12 m12" style="overflow: auto">
-                                        <div class="result" style="width:2500px;">
-                                            <table class="bordered" style="font-size:10px;">
-                                                <thead>
-                                                    <tr>
-                                                        <th>{{ __('translations.no') }}.</th>
-                                                        <th>No. Dokumen</th>
-                                                        <th>{{ __('translations.status') }}</th>
-                                                        <th>Voider</th>
-                                                        <th>Tgl. Void</th>
-                                                        <th>Ket. Void</th>
-                                                        <th>Deleter</th>
-                                                        <th>Tgl. Delete</th>
-                                                        <th>Ket. Delete</th>
-                                                        <th>Doner</th>
-                                                        <th>Tgl. Done</th>
-                                                        <th>Ket. Done</th>
-                                                        <th>NIK</th>
-                                                        <th>{{ __('translations.user') }}</th>
-                                                        <th>{{ __('translations.post_date') }}</th>
-                                                        <th>Status Kirim</th>
-                                                        <th>Tgl. Kirim</th>
-                                                        <th>Tipe Pengiriman</th>
-                                                        <th>Ekspedisi</th>
-                                                        <th>Pelanggan</th>
-                                                        <th>Kode Item</th>
-                                                        <th>{{ __('translations.item') }}</th>
-                                                        <th>Plant</th>
-                                                        <th>Qty Konversi</th>
-                                                        <th>Satuan Konversi</th>
-                                                        <th>Qty </th>
-                                                        <th>{{ __('translations.unit') }}</th>
-                                                        <th>Note Internal</th>
-                                                        <th>Note External</th>
-                                                        <th>Note </th>
-                                                        <th>No.SJ</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody id="detail_mod">
-                                                    <tr>
-                                                        <td class="center-align" colspan="20">Silahkan pilih tanggal dan tekan tombol filter.</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
 
+                    </div>
+                </div>
+
+                <div class="section">
+
+                    <div class="row">
+                        <div class="col s12 m12 l12" id="main-display">
+                            <ul class="collapsible collapsible-accordion">
+                                <li class="active">
+                                    <div class="collapsible-header"><i class="material-icons">filter_list</i>{{ __('translations.filter') }}</div>
+                                    <div class="collapsible-body">
+                                        <form class="row" id="form_data_filter" onsubmit="return false;">
+                                            <div class="col s12">
+                                                <div id="validation_alert_multi" style="display:none;"></div>
+                                            </div>
+                                            <div class="col s12">
+                                                <div class="row">
+                                                    <div class="input-field col m12 s12 step3">
+                                                        <select class="browser-default" id="item_id_multi" name="item_id_multi"  multiple></select>
+                                                        <label class="active" for="item_id_multi">Item RM / SM</label>
+                                                    </div>
+                                                    <div class="col m12 s12"></div>
+                                                    <div class="col m3 s6 ">
+                                                        <label for="start_date" style="font-size:1rem;">Tanggal Awal :</label>
+                                                        <input type="date" max="{{ date('9999'.'-12-31') }}" id="start_date_multi" name="start_date_multi" value="{{ date('Y-m-d') }}">
+                                                    </div>
+                                                    <div class="col m3 s6 ">
+                                                        <label for="finish_date" style="font-size:1rem;">Tanggal Akhir :</label>
+                                                        <input type="date" max="{{ date('9999'.'-12-31') }}" id="finish_date_multi" name="finish_date_multi" value="{{ date('Y-m-d') }}">
+                                                    </div>
+                                                    <div class="col m6 s6 pt-2">
+
+                                                        <a class="btn btn-small waves-effect waves-light breadcrumbs-btn mr-3" href="javascript:void(0);" onclick="whatPrintingPDF();">
+                                                            <i class="material-icons hide-on-med-and-up">picture_as_pdf</i>
+                                                            <span class="hide-on-small-onl">PDF</span>
+                                                            <i class="material-icons right">picture_as_pdf</i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            </div>
+                                        </form>
                                     </div>
-                                </div>
-                            </div>
-                        </div> --}}
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
 
@@ -159,6 +163,7 @@
 <script>
     $(function() {
         select2ServerSide('#item_id', '{{ url("admin/select2/item_rm_sm") }}');
+        select2ServerSide('#item_id_multi', '{{ url("admin/select2/item_rm_sm") }}');
     });
     function exportExcel(){
 
@@ -199,6 +204,43 @@
             '?start_date=' + encodeURIComponent(start_date) +
             '&finish_date=' + encodeURIComponent(end_date) +
             '&item_id=' + encodeURIComponent(item_id);
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            beforeSend: function () {
+                loadingOpen('.modal-content');
+            },
+            success: function (data) {
+                loadingClose('.modal-content');
+                console.log(data);
+                var link = document.createElement('a');
+                link.href = data.message;
+                link.download = data.file_name;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            },
+            error: function (xhr) {
+                loadingClose('.modal-content');
+                alert('Failed to generate ZIP. Please try again.');
+            }
+        });
+    }
+
+    function whatPrintingPDF() {
+        var item_id = $('#item_id_multi').val();
+        if (!item_id) {
+            alert("Item ID cannot be empty. Please select an item.");
+            return false;
+        }
+        var start_date = $('#start_date_multi').val();
+        var end_date = $('#finish_date_multi').val();
+
+        var url = '{{ Request::url() }}/print_multi_pdf' +
+            '?start_date=' + encodeURIComponent(start_date) +
+            '&finish_date=' + encodeURIComponent(end_date) +
+            '&item_multi=' + encodeURIComponent(item_id);
 
         $.ajax({
             url: url,

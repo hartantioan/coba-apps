17fb8d15c4a2e75b876af571e24d9efbdac006c0
Yustinus Windy <windykuro@gmail.com>
2025-02-19 10:44:52 +0700

    fix

--- app/Jobs/SendJournal.php
+++ app/Jobs/SendJournal.php
@@ -3038,7 +3038,7 @@ class SendJournal implements ShouldQueue
 							'line_id'                       => $rowcost->line_id ? $rowcost->line_id : ($row->asset->line_id ?? NULL),
 							'machine_id'                    => $rowcost->machine_id ? $rowcost->machine_id : ($row->asset->machine_id ?? NULL),
 							'department_id'                 => $rowcost->department_id ? $rowcost->department_id : ($row->asset->division_id ?? NULL),
-							'project_id'					=> $row->asset->place_id ?? NULL,
+							'project_id'					=> $row->asset->project_id ?? NULL,
 							'type'                          => '1',
 							'nominal'                       => $nominal,
 							'nominal_fc'					=> $nominal,

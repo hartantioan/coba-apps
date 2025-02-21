18be2db2ef7dc1df97c53894026bb495f70dc2c5
hartantioan <129711861+hartantioan@users.noreply.github.com>
2025-02-21 13:38:30 +0700

    Update MarketingOrderInvoiceDetail.php

--- app/Models/MarketingOrderInvoiceDetail.php
+++ app/Models/MarketingOrderInvoiceDetail.php
@@ -409,4 +409,15 @@ class MarketingOrderInvoiceDetail extends Model
             return $this;
         }
     }
+
+    public function getHPP()
+    {
+        if ($this->lookable_type == 'marketing_order_delivery_process_details') {
+            return  $this->lookable_type->total;
+        } else if ($this->lookable_type == 'marketing_order_delivery_details') {
+            return $this->lookable->marketingOrderDeliveryProcessDetail()->sum('total');
+        } else {
+            return 0;
+        }
+    }
 }

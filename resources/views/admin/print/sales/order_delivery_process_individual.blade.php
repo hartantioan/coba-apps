c79cd0ee725185565f366f4fcdc099cd28e51825
Yustinus Windy <windykuro@gmail.com>
2025-03-04 11:00:18 +0700

    Update order_delivery_process_individual.blade.php

--- resources/views/admin/print/sales/order_delivery_process_individual.blade.php
+++ resources/views/admin/print/sales/order_delivery_process_individual.blade.php
@@ -163,11 +163,11 @@
             </table>
         </header>
         <main style="margin-top:25px;">
-            {{-- @if ($data->marketingOrderDelivery->so_type == '4' || $data->marketingOrderDelivery->so_type == '3' )
+            @if ($data->marketingOrderDelivery->so_type == '4' || $data->marketingOrderDelivery->so_type == '3' )
                 <div style="position:absolute;top:50%;left:35%;width:180px;height:25px;padding:15px 15px 15px 15px;font-size:15px;text-align:center;border:1px solid black;border-radius:15px;opacity: 0.5;">
                     TIDAK UNTUK DIJUAL
                 </div>
-            @endif --}}
+            @endif
             <div class="card">
                 <div class="card-content invoice-print-area">
                     <!-- header section -->

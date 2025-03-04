0a87e627848b06341a97fe14cb7f664e3923ca90
hartantioan <129711861+hartantioan@users.noreply.github.com>
2025-03-04 14:08:17 +0700

    Update sample_test_input.blade.php

--- resources/views/admin/exports/sample_test_input.blade.php
+++ resources/views/admin/exports/sample_test_input.blade.php
@@ -49,21 +49,21 @@
                 $wet_white_val = $row->sampleTestResultQc->wet_whiteness_value ?? ''; // ✅ Handle null
                 $dry_white_val = $row->sampleTestResultQc->dry_whiteness_value ?? '';
                 $note = $row->sampleTestResultQc->note ?? '';
-                $link = asset(Storage::url($row->sampleTestResultQc->document)) ?? '';
+                $link = asset(Storage::url($row->sampleTestResultQc?->document)) ?? '';
             }
             elseif($row->type == 2){ // ✅ Use elseif instead of if
                 $nama_lab = $row->sampleTestResultProc->lab_name ?? '';
                 $wet_white_val = $row->sampleTestResultProc->wet_whiteness_value ?? '';
                 $dry_white_val = $row->sampleTestResultProc->dry_whiteness_value ?? '';
                 $note = $row->sampleTestResultProc->note ?? '';
-                $link = asset(Storage::url($row->sampleTestResultProc->document)) ?? '';
+                $link = asset(Storage::url($row->sampleTestResultProc?->document)) ?? '';
             }
             elseif($row->type == 3){ // ✅ Use elseif instead of if
                 $nama_lab = '';
                 $wet_white_val = '';
                 $dry_white_val = $row->sampleTestResultQcPacking->dry_whiteness_value ?? '';
                 $note = $row->sampleTestResultQcPacking->note ?? '';
-                $link = asset(Storage::url($row->sampleTestResultQcPacking->document)) ?? '';
+                $link = asset(Storage::url($row->sampleTestResultQcPacking?->document)) ?? '';
             }
             else {
                 $nama_lab = '';

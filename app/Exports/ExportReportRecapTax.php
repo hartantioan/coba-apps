cb54701ada4287e3824d1baa508917f959eec2ae
hartantioan <129711861+hartantioan@users.noreply.github.com>
2025-03-07 09:18:25 +0700

    Update ExportReportRecapTax.php

--- app/Exports/ExportReportRecapTax.php
+++ app/Exports/ExportReportRecapTax.php
@@ -104,11 +104,11 @@ class ExportReportRecapTax implements FromCollection, WithTitle, WithHeadings, S
                         }
                         if (date('Y-m-d', strtotime($row->created_at)) >= '2024-12-03') {
                             if ($row_detail->lookable->isPallet() || $row_detail->lookable->isBox()) {
-                                $boxQty = ' ( ' . CustomHelper::formatConditionalQty($row_detail->qty * $row_detail->lookable->item->pallet->box_conversion) . ' BOX )';
+                                $boxQty = ' ( ' . CustomHelper::formatConditionalQty($row_detail->qty * $row_detail->lookable->item->pallet?->box_conversion??1) . ' BOX )';
                             }
                         }else{
                             if ($row_detail->lookable->isPallet()) {
-                                $boxQty = ' ( ' . CustomHelper::formatConditionalQty($row_detail->qty * $row_detail->lookable->item->pallet->box_conversion) . ' BOX )';
+                                $boxQty = ' ( ' . CustomHelper::formatConditionalQty($row_detail->qty * $row_detail->lookable->item->pallet?->box_conversion??1) . ' BOX )';
                             }
                         }
 
@@ -116,7 +116,7 @@ class ExportReportRecapTax implements FromCollection, WithTitle, WithHeadings, S
                         if ($freeAreaTax) {
                             $hscode = ' ' . $row_detail->getHSCode();
                         }
-                        
+
                         if ($row_detail->is_include_tax == 1 && date('Y-m-d',strtotime($row_detail->getMarketingOrder->created_at)) < '2024-12-24') {
                             $percentTax = ($row_detail->getMarketingOrder->percent_tax + 100) / 100;
                         }
